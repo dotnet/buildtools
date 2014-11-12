65ab3377f77d07894a3521a1152697271175ca94
Matt Cohn <macohn@microsoft.com>
2014-11-11 22:46:11 -0800

    build.cmd uses either MSBuild 12 or MSBuild 14
    Previously our repositories would build in either a Dev12 or a Dev14
    Developer Command Prompt, but only if Dev12 (for MSBuild 12) was
    on the box due to a hard-coded path to MSBuild 12.  This change
    will use MSBuild 14 if MSBuild 12 is not available.

--- build.cmd
+++ build.cmd
@@ -9,6 +9,8 @@ setlocal
 :: Check prerequisites
 set _msbuildexe="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
 if not exist %_msbuildexe% set _msbuildexe="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
+if not exist %_msbuildexe% set _msbuildexe="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
+if not exist %_msbuildexe% set _msbuildexe="%ProgramFiles%\MSBuild\14.0\Bin\MSBuild.exe"
 if not exist %_msbuildexe% echo Error: Could not find MSBuild.exe.  Please see https://github.com/dotnet/corefx/blob/master/docs/Developers.md for build instructions. && goto :eof
 
 :: Log build command line
