4ada52dc5e8a8f17534946efb7dd1195c0e69f27
Eric St. John <ericstj@microsoft.com>
2015-04-21 16:31:07 -0700

    Prevent concurrent restore of same project.json
    DNX has an issue with concurrent restore: https://github.com/aspnet/dnx/issues/1682
    
    Previously I would workaround by copying to intermediate, but this breaks in official build where intermediate is not under the scope of our nuget.config, so custom package sources are ignored.
    
    Fix this instead by wrapping the call to dnu restore with a mutex unique to the project.json name.  We only need to do this for the test project.json because that is the only one we'll restore concurrently.

---
+++ src/Microsoft.DotNet.Build.Tasks/ExecWithMutex.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Tasks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    public class ExecWithMutex : Exec
+    {
+        [Required]
+        public string MutexName
+        {
+            get;
+            set;
+        }
+
+        protected override int ExecuteTool(string pathToTool, string responseFileCommands, string commandLineCommands)
+        {
+            string actualMutexName = MutexName.Replace(Path.DirectorySeparatorChar, '_');
+            bool created = false;
+
+            using (Mutex mutex = new Mutex(true, actualMutexName, out created))
+            {
+                try
+                {
+                    if (!created)
+                    {
+                        mutex.WaitOne();
+                    }
+                    return base.ExecuteTool(pathToTool, responseFileCommands, commandLineCommands);
+                }
+                finally
+                {
+                    mutex.ReleaseMutex();
+                }
+            }
+        }
+    }
+}
