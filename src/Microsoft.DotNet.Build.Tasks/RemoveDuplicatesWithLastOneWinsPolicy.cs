2343d6f36d96970b97cea6f210db90d54d2f7183
Nick Guerrera <nicholg@microsoft.com>
2015-06-18 13:31:17 -0700

    Move test deployment to build phase and speed it up
    The experience around building and running tests in VS got broken by a
    change to only copy tests when they're run from msbuild. Move them back
    to PrepareForRun to bring back the VS workflow.
    
    In doing so, it became apparent that the test deployment step was
    copying files on every invocation regardless of whether there were any
    changes. That turned out to be because there are different versions of
    files with the same simple name destind for the same output folder. As
    such, every iteration would swap them back and forth. The fix here is
    too trim out the duplicates before invoking the Copy task. Some of the
    duplication is by design (e.g. to override nuget assets with local
    assets), but we are also getting multiple nuget package versions out
    of the nuget asset resolution, which seems like a bug, which is now filed
    as #182.
    
    To further speed things up, using hard-links is now always on for the
    test deployment step, which currently copies tons of the same files
    to each test folder.
    
    Fix #181

---
+++ src/Microsoft.DotNet.Build.Tasks/RemoveDuplicatesWithLastOneWinsPolicy.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    /// <summary>
+    /// Given a list of items, remove duplicate items. Metadata is not considered. Case insensitive.
+    /// Unlike <see cref="RemoveDuplicates"/>, the last item in the list wins rather than the first.
+    /// That distinction is important when the items have different metadata.
+    /// </summary>
+    public class RemoveDuplicatesWithLastOneWinsPolicy : Task
+    {
+        /// <summary>
+        /// The list of items from which to remove duplicates.
+        /// </summary>
+        public ITaskItem[] Inputs
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The list of items with duplicates removed.
+        /// </summary>
+        [Output]
+        public ITaskItem[] Filtered
+        {
+            get;
+            set;
+        }
+
+        public override bool Execute()
+        {
+            var existingIndexMap = new Dictionary<string, int>(Inputs.Length, StringComparer.OrdinalIgnoreCase);
+            var filteredList = new List<ITaskItem>(Inputs.Length);
+
+            foreach (ITaskItem item in Inputs)
+            {
+                int existingIndex;
+                if (existingIndexMap.TryGetValue(item.ItemSpec, out existingIndex))
+                {
+                    filteredList[existingIndex] = item;
+                }
+                else
+                {
+                    filteredList.Add(item);
+                    existingIndexMap.Add(item.ItemSpec, filteredList.Count - 1);
+                }
+            }
+
+            Filtered = filteredList.ToArray();
+            return true;
+        }
+    }
+}
+
+
