2fc467dbda53568d295972c91219e3346bdfd5eb
Stephen A. Imhoff <clockwork-muse@outlook.com>
2015-08-07 15:10:07 +0900

    Stub base files.

---
+++ src/Microsoft.DotNet.Build.Tasks/GenerateEncodingTable.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    public class GenerateEncodingTable : Task
+    {
+        [Required]
+        public string inputPath { get; set; }
+
+        public override bool Execute()
+        {
+            return true;
+        }
+    }
+}
