7ad518a5cb861f081cd725d60452eaa9ffefee97
dagood <dagood@microsoft.com>
2015-12-01 18:49:35 -0600

    Remove UpdateDependencies target and refactor the package update task into two tasks.

--- src/Microsoft.DotNet.Build.Tasks/UpdateProjectDependencies.cs
+++ src/Microsoft.DotNet.Build.Tasks/UpdateProjectDependencies.cs
@@ -9,125 +9,14 @@ using System.Linq;
 
 namespace Microsoft.DotNet.Build.Tasks
 {
-    public class UpdateProjectDependencies : Task
+    public abstract class UpdateProjectDependencies : Task
     {
         [Required]
-        public ITaskItem ProjectJson { get; set; }
+        public ITaskItem[] ProjectJsons { get; set; }
 
-        public string OldPrerelease { get; set; }
-
-        public string NewPrerelease { get; set; }
-
-        public string PackageId { get; set; }
-
-        public string OldVersion { get; set; }
-
-        public string NewVersion { get; set; }
-
-        public override bool Execute()
+        private static JObject ReadProject(string projectJsonPath)
         {
-            var prereleaseUpdateParams = new[] { OldPrerelease, NewPrerelease }
-                .Select(param => !string.IsNullOrEmpty(param));
-
-            var versionUpdateParams = new[] { PackageId, OldVersion, NewVersion }
-                .Select(param => !string.IsNullOrEmpty(param));
-
-            bool anyPrereleaseUpdateParams = prereleaseUpdateParams.Any(p => p);
-            bool anyVersionUpdateParams = versionUpdateParams.Any(p => p);
-
-            if (anyPrereleaseUpdateParams ^ anyVersionUpdateParams)
-            {
-                if (prereleaseUpdateParams.All(p => p))
-                {
-                    ReplacePrerelease();
-                }
-                else if (versionUpdateParams.All(p => p))
-                {
-                    ReplacePackageVersion();
-                }
-                else
-                {
-                    if (anyPrereleaseUpdateParams)
-                    {
-                        Log.LogError("Not all properties found: expected (OldPrerelease, NewPrerelease)");
-                    }
-                    else
-                    {
-                        Log.LogError("Not all properties found: expected (PackageId, OldVersion, NewVersion)");
-                    }
-                }
-            }
-            else
-            {
-                Log.LogError(
-                    "Expected properties from one of (OldPrerelease, NewPrerelease) or (PackageId, OldVersion, NewVersion) but found {0}.",
-                    (anyPrereleaseUpdateParams && anyVersionUpdateParams) ? "properties from both" : "none");
-            }
-            return !Log.HasLoggedErrors;
-        }
-
-        public void ReplacePrerelease()
-        {
-            JObject projectRoot = ReadProject();
-            bool changed = false;
-
-            foreach (var package in projectRoot["dependencies"].OfType<JProperty>())
-            {
-                var dependencyIdentifier = package.Name;
-                var dependencyVersionRange = VersionRange.Parse(package.Value.ToObject<string>());
-                var dependencyVersion = dependencyVersionRange.MinVersion;
-
-                if (dependencyVersion.Release == OldPrerelease)
-                {
-                    Log.LogMessage(
-                        "Changing {0} {1} to {2} in {3}",
-                        dependencyIdentifier,
-                        dependencyVersion,
-                        NewPrerelease,
-                        ProjectJson);
-
-                    package.Value = dependencyVersionRange.OriginalString.Replace(OldPrerelease, NewPrerelease);
-                    changed = true;
-                }
-            }
-            if (changed)
-            {
-                WriteProject(projectRoot);
-            }
-        }
-
-        public void ReplacePackageVersion()
-        {
-            JObject projectRoot = ReadProject();
-            bool changed = false;
-
-            foreach (var package in projectRoot["dependencies"].OfType<JProperty>())
-            {
-                var dependencyIdentifier = package.Name;
-                var dependencyVersion = package.Value.ToObject<string>();
-
-                if (dependencyIdentifier == PackageId && dependencyVersion == OldVersion)
-                {
-                    Log.LogMessage(
-                        "Changing {0} {1} to {2} in {3}",
-                        dependencyIdentifier,
-                        dependencyVersion,
-                        NewVersion,
-                        ProjectJson);
-
-                    package.Value = NewVersion;
-                    changed = true;
-                }
-            }
-            if (changed)
-            {
-                WriteProject(projectRoot);
-            }
-        }
-
-        public JObject ReadProject()
-        {
-            using (TextReader projectFileReader = File.OpenText(ProjectJson.ItemSpec))
+            using (TextReader projectFileReader = File.OpenText(projectJsonPath))
             {
                 var projectJsonReader = new JsonTextReader(projectFileReader);
 
@@ -136,11 +25,94 @@ namespace Microsoft.DotNet.Build.Tasks
             }
         }
 
-        public void WriteProject(JObject projectRoot)
+        private static void WriteProject(JObject projectRoot, string projectJsonPath)
         {
             string projectJson = JsonConvert.SerializeObject(projectRoot, Formatting.Indented);
 
-            File.WriteAllText(ProjectJson.ItemSpec, projectJson + Environment.NewLine);
+            File.WriteAllText(projectJsonPath, projectJson + Environment.NewLine);
+        }
+
+        public abstract bool UpdatePackage(JProperty package, string projectJsonPath);
+
+        public override bool Execute()
+        {
+            foreach (var projectJsonPath in ProjectJsons.Select(item => item.ItemSpec))
+            {
+                JObject projectRoot = ReadProject(projectJsonPath);
+
+                bool changedAnyPackage = projectRoot["dependencies"]
+                    .OfType<JProperty>()
+                    .Any(package => UpdatePackage(package, projectJsonPath));
+
+                if (changedAnyPackage)
+                {
+                    WriteProject(projectRoot, projectJsonPath);
+                }
+            }
+
+            return !Log.HasLoggedErrors;
+        }
+    }
+
+    public class UpdatePrereleaseDependencies : UpdateProjectDependencies
+    {
+        [Required]
+        public string OldPrerelease { get; set; }
+
+        [Required]
+        public string NewPrerelease { get; set; }
+
+        public override bool UpdatePackage(JProperty package, string projectJsonPath)
+        {
+            var dependencyIdentifier = package.Name;
+            var dependencyVersionRange = VersionRange.Parse(package.Value.ToObject<string>());
+            var dependencyVersion = dependencyVersionRange.MinVersion;
+
+            if (dependencyVersion.Release == OldPrerelease)
+            {
+                Log.LogMessage(
+                    "Changing {0} {1} to {2} in {3}",
+                    dependencyIdentifier,
+                    dependencyVersion,
+                    NewPrerelease,
+                    projectJsonPath);
+
+                package.Value = dependencyVersionRange.OriginalString.Replace(OldPrerelease, NewPrerelease);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class UpdatePackageDependencyVersion : UpdateProjectDependencies
+    {
+        [Required]
+        public string PackageId { get; set; }
+
+        [Required]
+        public string OldVersion { get; set; }
+
+        [Required]
+        public string NewVersion { get; set; }
+
+        public override bool UpdatePackage(JProperty package, string projectJsonPath)
+        {
+            var dependencyIdentifier = package.Name;
+            var dependencyVersion = package.Value.ToObject<string>();
+
+            if (dependencyIdentifier == PackageId && dependencyVersion == OldVersion)
+            {
+                Log.LogMessage(
+                    "Changing {0} {1} to {2} in {3}",
+                    dependencyIdentifier,
+                    dependencyVersion,
+                    NewVersion,
+                    projectJsonPath);
+
+                package.Value = NewVersion;
+                return true;
+            }
+            return false;
         }
     }
 }
