d3ce0044ae0b077eb3eb2d20cef297c991808e45
Joel Hendrix <jhendrix@microsoft.com>
2015-12-03 11:08:45 -0800

    Add initial support for reporting build statistic.
    Generate a JSON file containing test build statistics and upload it to
    DocumentDB for later processing. At present, the only statistic captured
    is the number of successfully built tests.
    
    Stop adding xunit and corerun zip packages to the list of downloads as
    they are no longer required (they are pulled from packages.zip).

---
+++ src/Microsoft.DotNet.Build.CloudTestTasks/WriteBuildStatsJson.cs
@@ -0,0 +1,112 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Newtonsoft.Json;
+
+namespace Microsoft.DotNet.Build.CloudTestTasks
+{
+    /// <summary>
+    /// Used to write build stats to a JSON file.
+    /// </summary>
+    public class WriteTestBuildStatsJson : Task
+    {
+        /// <summary>
+        /// Helper class to be serialized to JSON.
+        /// </summary>
+        class TestBuildStatsJson
+        {
+            internal class FailedProject
+            {
+                public FailedProject(string name)
+                {
+                    ProjectName = name;
+                }
+
+                public string ProjectName { get; }
+            }
+
+            public TestBuildStatsJson(ITaskItem[] correlationIDs, string logUri, int builtCount, ITaskItem[] buildFailures, int testCount)
+            {
+                Aggregates = new List<string>();
+                ProjectsFailed = new List<FailedProject>();
+
+                foreach (var correlationId in correlationIDs)
+                    Aggregates.Add(correlationId.ItemSpec);
+
+                foreach (var buildFailure in buildFailures)
+                    ProjectsFailed.Add(new FailedProject(buildFailure.ItemSpec));
+
+                LogUri = logUri;
+                ProjectsBuiltCount = builtCount;
+                TestCount = testCount;
+            }
+
+            public List<string> Aggregates { get; }
+
+            public string LogUri { get; }
+
+            public int ProjectsBuiltCount { get; }
+
+            public List<FailedProject> ProjectsFailed { get; }
+
+            public string Stage { get { return "Build"; } }
+
+            public int TestCount { get; }
+        }
+
+        /// <summary>
+        /// The collection of correlation IDs to which these build stats apply.
+        /// </summary>
+        [Required]
+        public ITaskItem[] CorrelationIds { get; set; }
+
+        /// <summary>
+        /// The URI of the build log.
+        /// </summary>
+        [Required]
+        public string LogUri { get; set; }
+
+        /// <summary>
+        /// The name of the build stats JSON file to create.
+        /// </summary>
+        [Required]
+        public string OutputFile { get; set; }
+
+        /// <summary>
+        /// The number of projects successfully built.
+        /// </summary>
+        [Required]
+        public int ProjectsBuiltCount { get; set; }
+
+        /// <summary>
+        /// The collection of project names that failed to build.
+        /// </summary>
+        [Required]
+        public ITaskItem[] ProjectsFailed { get; set; }
+
+        /// <summary>
+        /// The total number of tests across all test binaries.
+        /// </summary>
+        [Required]
+        public int TestCount { get; set; }
+
+        public override bool Execute()
+        {
+            using (StreamWriter streamWriter = new StreamWriter(OutputFile))
+            using (JsonTextWriter jsonWriter = new JsonTextWriter(streamWriter))
+            {
+                jsonWriter.Formatting = Formatting.Indented;
+
+                var buildStats = new TestBuildStatsJson(CorrelationIds, LogUri, ProjectsBuiltCount, ProjectsFailed, TestCount);
+
+                var serializer = new JsonSerializer();
+                serializer.Serialize(jsonWriter, buildStats);
+            }
+            return true;
+        }
+    }
+}
