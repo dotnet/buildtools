e5d35cb4307c979ae651f669f92f154de2ca00ed
Eric St. John <ericstj@microsoft.com>
2015-12-03 12:25:41 -0800

    Use our own copy of GenerateNuSpec
    We needed a fix to GenerateNuSpec https://github.com/nuproj/nuproj/pull/176
    Rather than wait for a merge I've forked GenerateNuSpec.
    
    This is needed long-term because we plan to remove the
    NuProj Dependency entirely so that we can build on
    NETCore.

---
+++ src/Microsoft.DotNet.Build.Tasks.Packaging/src.Desktop/Extensions.cs
@@ -0,0 +1,181 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Build.Framework;
+using NuGet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.Versioning;
+
+namespace NuProj.Tasks
+{
+    public static class Extensions
+    {
+        private static readonly FrameworkName NullFramework = new FrameworkName("Null,Version=v1.0");
+
+        public static bool GetBoolean(this ITaskItem taskItem, string metadataName, bool defaultValue = false)
+        {
+            bool result = false;
+            var metadataValue = taskItem.GetMetadata(metadataName);
+            bool.TryParse(metadataValue, out result);
+            return result;
+        }
+
+        public static FrameworkName GetTargetFramework(this ITaskItem taskItem)
+        {
+            FrameworkName result = null;
+            var metadataValue = taskItem.GetMetadata(Metadata.TargetFramework);
+            if (!string.IsNullOrEmpty(metadataValue))
+            {
+                result = VersionUtility.ParseFrameworkName(metadataValue);
+            }
+            else
+            {
+                result = NullFramework;
+            }
+
+            return result;
+        }
+
+        public static FrameworkName GetTargetFrameworkMoniker(this ITaskItem taskItem)
+        {
+            FrameworkName result = null;
+            var metadataValue = taskItem.GetMetadata(Metadata.TargetFrameworkMoniker);
+            if (!string.IsNullOrEmpty(metadataValue))
+            {
+                result = new FrameworkName(metadataValue);
+            }
+            else
+            {
+                result = NullFramework;
+            }
+
+            return result;
+        }
+
+        public static PackageDirectory GetPackageDirectory(this ITaskItem taskItem)
+        {
+            var packageDirectoryName = taskItem.GetMetadata(Metadata.PackageDirectory);
+            if (string.IsNullOrEmpty(packageDirectoryName))
+            {
+                return PackageDirectory.Lib;
+            }
+
+            PackageDirectory result;
+            Enum.TryParse(packageDirectoryName, true, out result);
+            return result;
+        }
+
+        public static IVersionSpec GetVersion(this ITaskItem taskItem)
+        {
+            IVersionSpec result = null;
+            var metadataValue = taskItem.GetMetadata(Metadata.Version);
+            if (!string.IsNullOrEmpty(metadataValue))
+            {
+                VersionUtility.TryParseVersionSpec(metadataValue, out result);
+            }
+
+            return result;
+        }
+
+        public static IEnumerable<T> NullAsEmpty<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+            {
+                return Enumerable.Empty<T>();
+            }
+
+            return source;
+        }
+
+        public static string GetShortFrameworkName(this FrameworkName frameworkName)
+        {
+            if (frameworkName == null || frameworkName == NullFramework)
+            {
+                return null;
+            }
+
+            return VersionUtility.GetShortFrameworkName(frameworkName);
+        }
+
+        public static string ToStringSafe(this object value)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            return value.ToString();
+        }
+
+        public static void UpdateMember<T>(this T target, Expression<Func<T, string>> memberLamda, string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return;
+            }
+
+            var memberSelectorExpression = memberLamda.Body as MemberExpression;
+            if (memberSelectorExpression == null)
+            {
+                throw new InvalidOperationException("Invalid member expression.");
+            }
+            
+            var property = memberSelectorExpression.Member as PropertyInfo;
+            if (property == null)
+            {
+                throw new InvalidOperationException("Invalid member expression.");
+            }
+            
+            property.SetValue(target, value, null);
+        }
+
+        public static void AddRangeToMember<T, TItem>(this T target, Expression<Func<T, List<TItem>>> memberLamda, IEnumerable<TItem> value)
+        {
+            if (value == null || value.Count() == 0)
+            {
+                return;
+            }
+            
+            var memberSelectorExpression = memberLamda.Body as MemberExpression;
+            if (memberSelectorExpression == null)
+            {
+                throw new InvalidOperationException("Invalid member expression.");
+            }
+
+            var property = memberSelectorExpression.Member as PropertyInfo;
+            if (property == null)
+            {
+                throw new InvalidOperationException("Invalid member expression.");
+            }
+
+            var list = (List<TItem>)property.GetValue(target) ?? new List<TItem>();
+            list.AddRange(value);
+            
+            property.SetValue(target, list, null);
+        }
+
+        public static string Combine(this PackageDirectory packageDirectory, string targetFramework, string fileName)
+        {
+            switch (packageDirectory)
+            {
+                case PackageDirectory.Root:
+                    return fileName;
+                case PackageDirectory.Content:
+                    return Path.Combine(Constants.ContentDirectory, fileName);
+                case PackageDirectory.Build:
+                    return Path.Combine(Constants.BuildDirectory, fileName);
+                case PackageDirectory.Lib:
+                    return Path.Combine(Constants.LibDirectory, targetFramework, fileName);
+                case PackageDirectory.Tools:
+                    return Path.Combine(Constants.ToolsDirectory, fileName);
+                default:
+                    return fileName;
+            }
+        }
+    }
+}
---
+++ src/Microsoft.DotNet.Build.Tasks.Packaging/src.Desktop/Metadata.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace NuProj.Tasks
+{
+    public static class Metadata
+    {
+        public const string FileExclude = "Exclude";
+        
+        public const string FileSource = "FullPath";
+        
+        public const string FileTarget = "TargetPath";
+
+        public const string PackageDirectory = "PackageDirectory";
+        
+        public const string TargetFramework = "TargetFramework";
+
+        public const string TargetFrameworkMoniker = "TargetFrameworkMoniker";
+        
+        public const string Version = "Version";
+    }
+}
