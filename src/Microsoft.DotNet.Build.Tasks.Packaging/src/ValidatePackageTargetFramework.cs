b945c885429577adfc52e2df24e434f4e8cd8459
Eric St. John <ericstj@microsoft.com>
2015-12-09 21:49:46 -0800

    Eliminate nuproj dependency and refactor target sequence
    This eliminates the dependency on nuproj by copying the targets
    we were still using.  There is further trimming that can be done by
    removing work that is unnecessary but I will do that later.
    
    In addition to bringing these targets in I refactored the sequence
    of the targets so that we can build project references without actually
    having the binaries for the referenced project.  This will enable us to
    better separate the x-plat builds so that packages can come from
    from builds on multiple passes.

---
+++ src/Microsoft.DotNet.Build.Tasks.Packaging/src/ValidatePackageTargetFramework.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Build.Framework;
+using NuGet.Frameworks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.DotNet.Build.Tasks.Packaging
+{
+    public class ValidatePackageTargetFramework : PackagingTask
+    {
+        private Generations _generations;
+
+        [Required]
+        public string GenerationDefinitionsFile { get; set; }
+        
+        public string PackageTargetFramework { get; set; }
+
+        [Required]
+        public ITaskItem[] References { get; set; }
+
+        [Required]
+        public string[] ReferencePaths
+        {
+            get;
+            set;
+        }
+
+        public override bool Execute()
+        {
+            if (String.IsNullOrEmpty(PackageTargetFramework))
+            {
+                _log.LogMessage(LogImportance.Low, $"Skipping validation since PackageTargetFramework is not defined");
+                return true;
+            }
+
+            NuGetFramework fx = NuGetFramework.Parse(PackageTargetFramework);
+
+            if (fx.Framework != FrameworkConstants.FrameworkIdentifiers.NetPlatform)
+            {
+                _log.LogMessage(LogImportance.Low, $"Skipping validation since PackageTargetFramework {fx} is not {FrameworkConstants.FrameworkIdentifiers.NetPlatform}");
+                return true;
+            }
+
+            LoadGenerations();
+
+            foreach(var reference in References)
+            {
+                string path = reference.GetMetadata("FullPath");
+                
+                if (!File.Exists(path))
+                {
+                    _log.LogError($"Reference {path} does not exist.");
+                    continue;
+                }
+
+                var dependencyGeneration = _generations.DetermineGeneration(path, _log);
+
+                if (dependencyGeneration > fx.Version)
+                {
+                    _log.LogError($"Dependency {path} is generation {dependencyGeneration} which is greater than project generation {fx.Version}.");
+                }
+            }
+
+            return !_log.HasLoggedErrors;
+        }
+
+
+        private void LoadGenerations()
+        {
+            _generations = Generations.Load(GenerationDefinitionsFile);
+
+            _generations.ReferencePaths = ReferencePaths;
+        }
+
+    }
+}
