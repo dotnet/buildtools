2330727a2430e6ee7544b6cf22a8c8a93c8864cc
Mariana Rios Flores <mariari@microsoft.com>
2016-01-12 16:02:03 -0800

    Add Fxtransform target

---
+++ src/Microsoft.DotNet.Build.Tasks/PackageFiles/BlockReflectionAttribute.cs
@@ -0,0 +1,13 @@
+/*
+  Providing a definition for __BlockReflectionAttribute in an assembly is a signal to the .NET Native toolchain 
+  to remove the metadata for all non-public APIs. This both reduces size and disables private reflection on those 
+  APIs in libraries that include this. The attribute can also be applied to individual public APIs to similarly block them.
+  This file is consumed by Build.Common.Targets.
+*/
+
+using System;
+
+namespace System.Runtime.CompilerServices
+{
+    internal class __BlockReflectionAttribute : Attribute { }
+}
