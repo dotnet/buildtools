5c1356703a5b085150f675334ff96497eb9b9d3d
Matt Mitchell <mmitche@microsoft.com>
2016-02-17 20:32:35 -0800

    Revert "[Do not Merge] Simplify and move to the branch CI model"

--- netci.groovy
+++ netci.groovy
@@ -2,27 +2,33 @@
 
 import jobs.generation.Utilities;
 
-def project = GithubProject
-def branch = GithubBranchName
-
+def project = 'dotnet/buildtools'
 // Define build string
 def buildString = '''call "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\Tools\\VsDevCmd.bat" && build.cmd /p:SkipTests=true'''
 
 // Generate the builds for debug and release
 
-[true, false].each { isPR ->
-    def newJob = job(Utilities.getFullJobName(project, '', false)) {
-      steps {
-        batchFile(buildString)
-      }
-    }
-    
-    Utilities.setMachineAffinity(newJob, 'Windows_NT', 'latest-or-auto')
-    Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
-    if (isPR) {
-        Utilities.addGithubPRTriggerForBranch(newJob, branch, 'Innerloop Windows Debug')
-    }
-    else {
-        Utilities.addGithubPushTrigger(newJob)
-    }
+def commitJob = job(Utilities.getFullJobName(project, '', false)) {
+  label('windows')
+  steps {
+    batchFile(buildString)
+  }
 }
+             
+def PRJob = job(Utilities.getFullJobName(project, '', true)) {
+  label('windows')
+  steps {
+    batchFile(buildString)
+  }
+}
+
+Utilities.addScm(commitJob, project)
+Utilities.addStandardOptions(commitJob)
+Utilities.addStandardNonPRParameters(commitJob)
+Utilities.addGithubPushTrigger(commitJob)
+             
+
+Utilities.addPRTestSCM(PRJob, project)
+Utilities.addStandardOptions(PRJob)
+Utilities.addStandardPRParameters(PRJob, project)
+Utilities.addGithubPRTrigger(PRJob, 'Windows Debug')
