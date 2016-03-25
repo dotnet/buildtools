a45e64a818e06cd45add25ee8780afad10a7e281
Lakshmi Priya Sekar <lasekar@microsoft.com>
2016-03-25 12:14:33 -0700

    Respond to PR feedback.

--- src/Microsoft.DotNet.Build.Tasks/GetTargetMachineInfo.cs
+++ src/Microsoft.DotNet.Build.Tasks/GetTargetMachineInfo.cs
@@ -11,13 +11,32 @@ using System.Linq;
 
 namespace Microsoft.DotNet.Build.Tasks
 {
-    public class GetTargetOS : Task
+    public class GetTargetMachineInfo : Task
     {
         [Output]
         public string TargetOS { get; set; }
 
+        [Output]
+        public string TargetArch { get; set; }
+
         public override bool Execute()
         {
+            switch (RuntimeInformation.OSArchitecture)
+            {
+                case Architecture.X64:
+                    TargetArch = "x64";
+                    break;
+                case Architecture.X86:
+                    TargetArch = "x86";
+                    break;
+                case Architecture.Arm:
+                    TargetArch = "arm";
+                    break;
+                case Architecture.Arm64:
+                    TargetArch = "arm64";
+                    break;
+            }
+
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 TargetOS = "Windows_NT";
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
@@ -28,6 +47,18 @@ namespace Microsoft.DotNet.Build.Tasks
                 TargetOS = "FreeBSD";
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Create("NETBSD")))
                 TargetOS = "NetBSD";
+
+            if (TargetArch == null)
+            {
+                Log.LogError("{0} is null", nameof(TargetArch));
+                return false;
+            }
+
+            if (TargetOS == null)
+            {
+                Log.LogError("{0} is null", nameof(TargetOS));
+                return false;
+            }
 
             return true;
         }
