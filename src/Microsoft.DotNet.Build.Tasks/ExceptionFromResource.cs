fb78852f73747a6854df7fc8d32eec68b7c1c2d0
Eric Mellino <erme@microsoft.com>
2016-04-05 13:54:23 -0700

    Update resolve assets to support project references
    * Remove the separated 'project' and 'test-runtime' concepts. Each
      individual test project must now reference a 'test-runtime' project
      so that their assets can be resolved together.

---
+++ src/Microsoft.DotNet.Build.Tasks/ExceptionFromResource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    internal sealed class ExceptionFromResource : Exception
+    {
+        public string ResourceName { get; private set; }
+        public object[] MessageArgs { get; private set; }
+
+        public ExceptionFromResource(string resourceName, params object[] messageArgs)
+        {
+            ResourceName = resourceName;
+            MessageArgs = messageArgs;
+        }
+    }
+}
