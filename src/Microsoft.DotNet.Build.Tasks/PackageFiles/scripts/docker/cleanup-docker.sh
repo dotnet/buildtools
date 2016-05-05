b57598199ba7f81b859dc28930e6c5c23f473b49
Christopher Costa <chcosta@microsoft.com>
2016-05-05 12:40:00 -0700

    Add comments

--- src/Microsoft.DotNet.Build.Tasks/PackageFiles/scripts/docker/cleanup-docker.sh
+++ src/Microsoft.DotNet.Build.Tasks/PackageFiles/scripts/docker/cleanup-docker.sh
@@ -8,6 +8,7 @@
 my $psList = `docker ps -a`;
 my @psItems = split /\n/, $psList;
 foreach(@psItems) {
+  # match 'docker ps' output to capture the container name
   if($_ =~ /.*\s+([^\s]+)$/ig) {
     my $containerName = $1;
     if($containerName !~ /NAME/ig) {
@@ -22,6 +23,7 @@ foreach(@psItems) {
 my $imageList = `docker images`;
 @imageItems = split /\n/, $imageList;
 foreach(@imageItems) {
+  # match 'docker images' output to capture the image id
   if($_ =~ /([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+.*/ig) {
     my $imageId = $3;
     if($imageId !~ /IMAGE/ig) {
