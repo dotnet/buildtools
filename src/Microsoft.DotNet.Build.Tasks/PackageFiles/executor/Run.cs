4929eca1ca26c4dbb0674f6a4a232660a56b9cd1
Mariana Rios Flores <mariari@microsoft.com>
2016-05-11 11:57:03 -0700

    Process running with dev workflow steps only for windows

---
+++ src/Microsoft.DotNet.Build.Tasks/PackageFiles/executor/Run.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Text;
+
+namespace Microsoft.DotNet.Execute
+{
+    public class Run
+    {
+        private System.Diagnostics.Process _process;
+        
+        public int ExecuteProcess(string filename, string args = null)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = filename,
+                Arguments = args,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+
+            _process = new System.Diagnostics.Process();
+            _process.StartInfo = psi;
+
+            // Set our event handler to asynchronously read the output.
+            _process.OutputDataReceived += new DataReceivedEventHandler(ReadOutputHandler);
+
+            _process.Start();
+            _process.BeginOutputReadLine();
+            
+            _process.WaitForExit();
+            return _process.ExitCode;
+        }
+
+        private static void ReadOutputHandler(object sendingProcess,
+            DataReceivedEventArgs outLine)
+        {
+            // Collect the sort command output.
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                Console.WriteLine(outLine.Data);
+            }
+        }
+
+    }
+
+
+            
+}
