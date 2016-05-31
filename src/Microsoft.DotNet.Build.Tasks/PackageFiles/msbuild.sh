a81dd038272e9c537900a8af8cdc77974aa27dd8
Mariana Rios Flores <mariari@microsoft.com>
2016-05-31 16:14:43 -0700

    adding changes to msbuild.sh

--- src/Microsoft.DotNet.Build.Tasks/PackageFiles/msbuild.sh
+++ src/Microsoft.DotNet.Build.Tasks/PackageFiles/msbuild.sh
@@ -4,7 +4,6 @@ working_tree_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $working_tree_root/corerun $working_tree_root/MSBuild.exe $*
 $working_tree_root/corerun $working_tree_root/MSBuild.exe $*
 if [ $? -ne 0 ]; then
-    echo Error!
-    exit 1 
+  exit 1 
 fi
 exit 0
