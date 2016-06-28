2fff35ab5e81067aa1809b8f6606f65d7350523a
Mariana Rios Flores <mariari@microsoft.com>
2016-06-28 10:35:04 -0700

    small fixes according to feedback

--- src/Microsoft.DotNet.Build.Tasks/PackageFiles/msbuild.cmd
+++ src/Microsoft.DotNet.Build.Tasks/PackageFiles/msbuild.cmd
@@ -1,8 +1,2 @@
-@if "%_echo%" neq "on" echo off
-setlocal
-echo msbuild.exe %*
 call msbuild.exe %*
-IF ERRORLEVEL 1 (
-  exit /b 1
-)
-exit /b 0
+exit /b %ERRORLEVEL% 
--- src/Microsoft.DotNet.Build.Tasks/PackageFiles/msbuild.sh
+++ src/Microsoft.DotNet.Build.Tasks/PackageFiles/msbuild.sh
@@ -3,7 +3,4 @@
 working_tree_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $working_tree_root/corerun $working_tree_root/MSBuild.exe $*
 $working_tree_root/corerun $working_tree_root/MSBuild.exe $*
-if [ $? -ne 0 ]; then
-  exit 1 
-fi
-exit 0
+exit $ 
