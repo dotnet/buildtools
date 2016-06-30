833f8f721d13586e00bacc6161ff59145e648369
Mariana Rios Flores <mariari@microsoft.com>
2016-06-29 17:23:21 -0700

    Merge pull request #672 from maririos/parser - The "run" tool - dev workflow
    The run tool has a published contract of inputs and outputs to encapsulate the work we've been doing related to the dev workflow. It parses arguments and formats them in order to run the command given by the user. It also provides documentation of the most common settings we use per repo, and the commands we can execute.
    Each command has a set of options/alias that will trigger a different behavior.

---
+++ src/Run/RunProcess.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Microsoft.DotNet.Execute
+{
+    public static class RunProcess
+    {
+        private static System.Diagnostics.Process _process;
+        private static bool _exited = false;
+
+        public static int ExecuteProcess(string filename, string args = null)
+        {
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = filename,
+                    Arguments = args,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false
+                };
+
+                _process = new System.Diagnostics.Process();
+                _process.StartInfo = psi;
+                _process.EnableRaisingEvents = true;
+
+                // Set the event handler to asynchronously read the output.
+                _process.OutputDataReceived += new DataReceivedEventHandler(ReadOutputHandler);
+                
+                _process.Start();
+                _process.BeginOutputReadLine();
+                string errorOutput = _process.StandardError.ReadToEnd();
+
+                _process.WaitForExit();
+                Console.Error.WriteLine(errorOutput);
+
+                const int SLEEP_AMOUNT = 1000;
+                while (!_exited)
+                {
+                    Thread.Sleep(SLEEP_AMOUNT);
+                }
+                return _process.ExitCode;
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+                return 1;
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Error in the command: {0}. => {1}", string.Format("{0} {1}", filename, args), e.Message);
+                return 1;
+            }
+        }
+
+        private static void ReadOutputHandler(object sendingProcess,
+            DataReceivedEventArgs outLine)
+        {
+            // Collect the command output.
+            if (outLine.Data != null)
+            {
+                Console.WriteLine(outLine.Data);
+            }
+            else
+            {
+                _exited = true;
+            }
+        }
+    }
+}
