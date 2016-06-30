e1f782713d2fb52f6f40262fe730d6d296236763
Mariana Rios Flores <mariari@microsoft.com>
2016-06-29 17:12:05 -0700

    delede echo in msbuild.sh

--- src/Microsoft.DotNet.Build.Tasks/PackageFiles/msbuild.sh
+++ src/Microsoft.DotNet.Build.Tasks/PackageFiles/msbuild.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 working_tree_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo $working_tree_root/corerun $working_tree_root/MSBuild.exe $*
 $working_tree_root/corerun $working_tree_root/MSBuild.exe $*
 exit $ 
