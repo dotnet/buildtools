dba72dff076fb09ba0e1ddc6d4580c7b1d27adbc
Lakshmi Priya Sekar <lasekar@microsoft.com>
2016-07-12 14:53:02 -0700

    Add logic to Get values from SettingValueProvider.

---
+++ src/Run/Constants.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.DotNet.Execute
+{
+    public struct OS
+    {
+        public const string OSX = "OSX";
+        public const string FreeBSD = "FreeBSD";
+        public const string NetBSD = "NetBSD";
+        public const string Linux = "Linux";
+        public const string Unix = "Unix";
+        public const string Windows = "Windows_NT";
+    }
+
+    public struct Arch
+    {
+        public const string X86 = "x86";
+        public const string X64 = "x64";
+        public const string Arm = "arm";
+        public const string Arm64 = "arm64";
+    }
+}
--- src/Run/SettingValueProvider.cs
+++ src/Run/SettingValueProvider.cs
@@ -5,14 +5,31 @@
 using System.Runtime.InteropServices;
 using System.IO;
 using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Microsoft.DotNet.Execute
 {
-    internal class SettingValueProvider
+    internal static class SettingValueProvider
     {
         private static string s_cpuArch = null;
         private static string s_osName = null;
+        private static string s_osVersion = null;
+        private static Dictionary<string, string> s_osToDefaultVersionMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+        {
+            { OS.OSX, "osx.10.10" },
+            { OS.FreeBSD, "osx.10.10" },
+            { OS.NetBSD, "osx.10.10" },
+            { OS.Linux, "ubuntu.14.04" },
+            { OS.Windows, string.Empty }
+        };
 
+        public static string Get(string settingName)
+        {
+            PropertyInfo property = typeof(SettingValueProvider).GetTypeInfo().GetProperty(settingName);
+            return (property != null) ? (string)property.GetValue(null) : string.Empty;
+        }
+        
         public static string CPUArch
         {
             get
@@ -22,28 +39,63 @@ namespace Microsoft.DotNet.Execute
                     switch (RuntimeInformation.OSArchitecture)
                     {
                         case Architecture.X86:
-                            s_cpuArch = "x86";
-                            break;
-
-                        case Architecture.X64:
-                            s_cpuArch = "x64";
+                            s_cpuArch = Arch.X86;
                             break;
 
                         case Architecture.Arm:
-                            s_cpuArch = "arm";
+                            s_cpuArch = Arch.Arm;
                             break;
 
                         case Architecture.Arm64:
-                            s_cpuArch = "arm64";
+                            s_cpuArch = Arch.Arm64;
                             break;
 
+                        case Architecture.X64:
                         default:
-                            s_cpuArch = "x64";
+                            s_cpuArch = Arch.X64;
                             break;
                     }
                 }
 
                 return s_cpuArch;
+            }
+        }
+
+        public static string OSVersion
+        {
+            get
+            {
+                if (s_osVersion == null)
+                {
+                    switch (OSName)
+                    {
+                        case OS.Linux:
+                            const string OSReleaseFileName = @"/etc/os-release";
+                            if (File.Exists(OSReleaseFileName))
+                            {
+                                string content = File.ReadAllText(OSReleaseFileName);
+                                int idIndex = content.IndexOf("ID");
+                                int versionIndex = content.IndexOf("VERSION_ID");
+                                if (idIndex != -1 && versionIndex != -1)
+                                {
+                                    string id = content.Substring(idIndex + 3, content.IndexOf(Environment.NewLine, idIndex + 3) - idIndex - 3);
+                                    string version = content.Substring(versionIndex + 12, content.IndexOf('"', versionIndex + 12) - versionIndex - 12);
+                                    s_osVersion = $"{id}.{version}";
+                                }
+                                else
+                                {
+                                    s_osVersion = s_osToDefaultVersionMap[OS.Linux];
+                                }
+                            }
+                            break;
+
+                        default:
+                            s_osVersion = s_osToDefaultVersionMap[OSName];
+                            break;
+                    }
+                }
+
+                return s_osVersion;
             }
         }
 
@@ -55,39 +107,23 @@ namespace Microsoft.DotNet.Execute
                 {
                     if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
                     {
-                        s_osName = "OSX";
+                        s_osName = OS.OSX;
                     }
                     else if (RuntimeInformation.IsOSPlatform(OSPlatform.Create("FREEBSD")))
                     {
-                        s_osName = "FreeBSD";
+                        s_osName = OS.FreeBSD;
                     }
                     else if (RuntimeInformation.IsOSPlatform(OSPlatform.Create("NETBSD")))
                     {
-                        s_osName = "NetBSD";
+                        s_osName = OS.NetBSD;
                     }
                     else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
                     {
-                        const string OSReleaseFileName = @"/etc/os-release";
-                        if (File.Exists(OSReleaseFileName))
-                        {
-                            string content = File.ReadAllText(OSReleaseFileName);
-                            int idIndex = content.IndexOf("ID");
-                            int versionIndex = content.IndexOf("VERSION_ID");
-                            if (idIndex != -1 && versionIndex != -1)
-                            {
-                                string id = content.Substring(idIndex + 3, content.IndexOf(Environment.NewLine, idIndex + 3) - idIndex - 3);
-                                string version = content.Substring(versionIndex + 12, content.IndexOf('"', versionIndex + 12) - versionIndex - 12);
-                                s_osName = $"{id}.{version}";
-                            }
-                            else
-                            {
-                                s_osName = "ubuntu.14.04";
-                            }
-                        }
+                        s_osName = OS.Linux;
                     }
                     else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                     {
-                        s_osName = "Windows_NT";
+                        s_osName = OS.Windows;
                     }
                 }
 
