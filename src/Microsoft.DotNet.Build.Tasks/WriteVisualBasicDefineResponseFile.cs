cf1b15fc9272636d44f1dbb16e06f9e90070fd1d
Matt Ellis <matell@microsoft.com>
2016-08-09 16:54:37 -0700

    Add a task to help workaround dotnet/corefx#5230
    This task generates a response file we can use to pass the correct set of
    defines to the complier without having to worry about MSBuild converting
    back slashes to forward slashes when running on Unix systems.

---
+++ src/Microsoft.DotNet.Build.Tasks/WriteVisualBasicDefineResponseFile.cs
@@ -0,0 +1,45 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Newtonsoft.Json;
+using System.IO;
+
+/// <summary>
+/// Writes a response file that sets the define flag to the Visual Basic complier
+/// based on what constants should be defined. This is needed due to 
+/// https://github.com/Microsoft/msbuild/issues/422 which prevents us from
+/// using the native MSBuild logic to invoke the build for our VB Projects when
+/// running on Unix platforms.
+/// </summary>
+namespace Microsoft.DotNet.Build.Tasks
+{
+    public sealed class WriteVisualBasicDefineResponseFile : Task
+    {
+        /// <summary>
+        /// The set of DefineConstants that would be passed to the Vbc Task.
+        /// </summary>
+        [Required]
+        public string DefineConstants { get; set; }
+
+        /// <summary>
+        /// The response file to generate. If this file already exists, it is overwritten.
+        /// </summary>
+        [Required]
+        public string File { get; set; }
+
+        public override bool Execute()
+        {
+            using (var streamWriter = new StreamWriter(System.IO.File.OpenWrite(File)))
+            {
+                streamWriter.Write("/define:\"");
+                streamWriter.Write(DefineConstants.Replace("\"", "\\\""));
+                streamWriter.Write("\"");
+            }
+
+            return true;
+        }
+    }
+}
