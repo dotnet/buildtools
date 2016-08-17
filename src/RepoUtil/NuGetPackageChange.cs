dac4c47a6e2f66cbd994858e8c5d77daa070966e
Joel Hendrix <jhendrix@microsoft.com>
2016-08-17 11:42:56 -0700

    Add RepoUtil sources to the build tools repo.
    We have a tool, RepoUtil, created by the Roslyn team for implementing the
    produces and consumes repo APIs.  Since this is meant to be a general
    purpose tool to be consumed by various repos I'm moving the sources to the
    build tools repo.  I have also added nuget package authoring for this tool
    so that it can easily be consumed by other repos.  In the process I have
    converted the tool to be a .NET Core app.

---
+++ src/RepoUtil/NuGetPackageChange.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace RepoUtil
+{
+    internal sealed class NuGetPackageChange
+    {
+        internal string Name { get; }
+        internal string OldVersion { get; }
+        internal string NewVersion { get; }
+        internal NuGetPackage OldPackage => new NuGetPackage(Name, OldVersion);
+        internal NuGetPackage NewPackage => new NuGetPackage(Name, NewVersion);
+
+        internal NuGetPackageChange(string name, string oldVersion, string newVersion)
+        {
+            Name = name;
+            OldVersion = oldVersion;
+            NewVersion = newVersion;
+        }
+
+        public override string ToString() => $"{Name} from {OldVersion} to {NewVersion}";
+    }
+}
---
+++ src/RepoUtil/NuSpecUtil.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace RepoUtil
+{
+    internal static class NuSpecUtil
+    {
+        internal static IEnumerable<FileName> GetNuSpecFiles(string sourcesPath)
+        {
+            return Directory
+                .EnumerateFiles(sourcesPath, "*.nuspec", SearchOption.AllDirectories)
+                .Select(x => FileName.FromFullPath(sourcesPath, x));
+        }
+
+        internal static string GetId(string nuspecFilePath)
+        {
+            var doc = XDocument.Load(nuspecFilePath);
+            var ns = XNamespace.Get("http://schemas.microsoft.com/packaging/2011/08/nuspec.xsd");
+            var id = doc
+                .Element(ns.GetName("package"))
+                .Element(ns.GetName("metadata"))
+                .Element(ns.GetName("id"))
+                .Value;
+            return id;
+        }
+    }
+}
