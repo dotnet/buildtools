82d9d75ef5ef46ea7b344306a9d8a3b2abbfb417
Christopher Costa <chcosta@microsoft.com>
2016-09-13 09:05:03 -0700

    Produces MSBuild task

---
+++ src/Microsoft.DotNet.VersionTools/Produces/Produces.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NuGet.Packaging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Microsoft.DotNet.VersionTools.Produces
+{
+    public class Produces : Task
+    {
+        internal static readonly string PackageExtension = ".nupkg";
+        internal static readonly string SymbolsExtension = ".symbols";
+
+        [Required]
+        public string Folder { get; set; }
+        [Required]
+        public string OutputFile { get; set; }
+        public override bool Execute()
+        {
+            var artifacts = Directory.GetFiles(Folder, "*", SearchOption.AllDirectories).ToDictionary(f => f, f => Path.GetFileName(f));
+            var clashes = artifacts.GroupBy(a => a.Value).Where(a => a.Count() > 1);
+            if(clashes.Count() > 0)
+            {
+                foreach(var clash in clashes)
+                {
+                    Console.Error.WriteLine("Error: Artifact names must be unique.  Artifact '{0}' found in '{1}'.", clash.Key, string.Join(",", clash.Select(c => c.Key).ToArray()));
+                }
+                return false;
+            }
+            
+            var packages = new JProperty("packages", 
+                                         new JObject(artifacts.Keys.Where(a => Path.GetExtension(a).Equals(PackageExtension) && !a.Contains(SymbolsExtension)).Select(a => CreatePackageJProperty(a))));
+            var symbolsPackages = new JProperty("symbols-packages",
+                                         new JObject(artifacts.Keys.Where(a => Path.GetExtension(a).Equals(PackageExtension) && a.Contains(SymbolsExtension)).Select(a => CreatePackageJProperty(a))));
+            var files = new JProperty("files", JArray.FromObject(artifacts.Values.Where(a => !Path.GetExtension(a).Equals(PackageExtension))));
+
+            JObject obj = new JObject();
+            if (packages != null && packages.First.HasValues)
+            {
+                obj.Add(packages);
+            }
+            if(symbolsPackages != null && symbolsPackages.First.HasValues)
+            {
+                obj.Add(symbolsPackages);
+            }
+            if (files != null && files.First.HasValues)
+            {
+                obj.Add(files);
+            }
+
+            var data = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            File.WriteAllText(OutputFile, data);
+
+            return true;
+        }
+        private JProperty CreatePackageJProperty(string nupkg)
+        {
+            PackageArchiveReader archive = new PackageArchiveReader(nupkg);
+
+            var id = archive.GetIdentity();
+            
+            return new JProperty(id.Id, id.Version.ToString());
+        }
+    }
+}
