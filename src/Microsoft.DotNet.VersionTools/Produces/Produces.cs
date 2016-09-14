519b056f600d477d3fdfca2a7f95d36d8e27be93
Christopher Costa <chcosta@microsoft.com>
2016-09-14 14:36:07 -0700

    Add support for grouping, allow root produces folder to be overridden

--- src/Microsoft.DotNet.VersionTools/Produces/Produces.cs
+++ src/Microsoft.DotNet.VersionTools/Produces/Produces.cs
@@ -22,43 +22,51 @@ namespace Microsoft.DotNet.VersionTools.Produces
 
         public override bool Execute()
         {
-            var artifacts = Directory.GetFiles(Folder, "*", SearchOption.AllDirectories).ToDictionary(f => f, f => Path.GetFileName(f));
-            var clashes = artifacts.GroupBy(a => a.Value).Where(a => a.Count() > 1);
-            if (clashes.Count() > 0)
+            JObject objectRoot = new JObject();
+            foreach (var folder in Directory.GetDirectories(Folder))
             {
-                foreach (var clash in clashes)
+                var grouping = Path.GetFileName(folder);
+                var artifacts = Directory.GetFiles(folder, "*", SearchOption.AllDirectories).ToDictionary(f => f, f => Path.GetFileName(f));
+                var clashes = artifacts.GroupBy(a => a.Value).Where(a => a.Count() > 1);
+                if (clashes.Count() > 0)
                 {
-                    Console.Error.WriteLine("Error: Artifact names must be unique.  Artifact '{0}' found in '{1}'.", clash.Key, string.Join(",", clash.Select(c => c.Key).ToArray()));
+                    foreach (var clash in clashes)
+                    {
+                        Console.Error.WriteLine("Error: Artifact names must be unique per grouping ('{0}').  Artifact '{1}' found in '{2}'.", grouping, clash.Key, string.Join(",", clash.Select(c => c.Key).ToArray()));
+                    }
+                    return false;
                 }
-                return false;
+                var packages = new JProperty("packages",
+                                             new JObject(artifacts.Keys.Where(a => Path.GetExtension(a).Equals(PackageExtension) && !a.Contains(SymbolsExtension)).Select(a => CreatePackageJProperty(a))));
+                var symbolsPackages = new JProperty("symbols-packages",
+                                                    new JObject(artifacts.Keys.Where(a => Path.GetExtension(a).Equals(PackageExtension) && a.Contains(SymbolsExtension)).Select(a => CreatePackageJProperty(a))));
+                var nuget = new JProperty("nuget", new JArray() { new JObject(packages), new JObject(symbolsPackages) });
+
+                JObject obj = new JObject();
+
+                if ((packages != null && packages.First.HasValues) || (
+                    (symbolsPackages != null && symbolsPackages.First.HasValues)))
+                {
+                    obj.Add(nuget);
+                }
+
+                var files = new JProperty("files", JArray.FromObject(artifacts.Values.Where(a => !Path.GetExtension(a).Equals(PackageExtension))));
+                if (files != null && files.First.HasValues)
+                {
+                    obj.Add(files);
+                }
+                if (obj.Count > 0)
+                {
+                    objectRoot.Add(grouping, obj);
+                }
             }
-            var packages = new JProperty("packages",
-                                         new JObject(artifacts.Keys.Where(a => Path.GetExtension(a).Equals(PackageExtension) && !a.Contains(SymbolsExtension)).Select(a => CreatePackageJProperty(a))));
-            var symbolsPackages = new JProperty("symbols-packages",
-                                                new JObject(artifacts.Keys.Where(a => Path.GetExtension(a).Equals(PackageExtension) && a.Contains(SymbolsExtension)).Select(a => CreatePackageJProperty(a))));
-            var nuget = new JProperty("nuget", new JArray() { new JObject(packages), new JObject(symbolsPackages) });
-
-            JObject obj = new JObject();
-
-            if ((packages != null && packages.First.HasValues) || (
-                (symbolsPackages != null && symbolsPackages.First.HasValues)))
-            {
-                obj.Add(nuget);
-            }
-
-            var files = new JProperty("files", JArray.FromObject(artifacts.Values.Where(a => !Path.GetExtension(a).Equals(PackageExtension))));
-            if (files != null && files.First.HasValues)
-            {
-                obj.Add(files);
-            }
-
-            if (obj.Count == 0)
+            if (objectRoot.Count == 0)
             {
                 Console.WriteLine("Produces did not generate any output. No artifacts found at {0}, skipping writing {1}", Folder, OutputFile);
             }
             else
             {
-                var data = JsonConvert.SerializeObject(obj, Formatting.Indented);
+                var data = JsonConvert.SerializeObject(objectRoot, Formatting.Indented);
                 File.WriteAllText(OutputFile, data);
             }
             return true;
