fc73207bf88d5d16825b94c45280c86d2ec0ec3a
Joel Hendrix <jhendrix@microsoft.com>
2016-09-15 10:44:14 -0700

    Update repo to use the new build tools boot-straper script.
    Use the new script, bootstrap.ps1, to acquire and initialize packages
    required to build the repo.
    Fixed a bug in bootstrap.ps1 thas was deleting the script after it was
    downloaded, and verify that the package that was restored matches the
    expected version.

--- bootstrap/bootstrap.ps1
+++ bootstrap/bootstrap.ps1
@@ -26,9 +26,13 @@ $initCliLocalPath = Join-Path $ToolsLocalPath $initCliScript
 # blow away the tools directory so we can start from a known state
 if (Test-Path $ToolsLocalPath)
 {
-    rd -recurse -force $ToolsLocalPath
+    # if the bootstrap.ps1 script was downloaded to the tools directory don't delete it
+    rd -recurse -force $ToolsLocalPath -exclude "bootstrap.ps1"
 }
-mkdir $ToolsLocalPath | Out-Null
+else
+{
+    mkdir $ToolsLocalPath | Out-Null
+}
 
 # download CLI boot-strapper script
 Invoke-WebRequest "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1" -OutFile $initCliLocalPath
@@ -86,6 +90,14 @@ foreach ($tool in $tools)
 {
     $name, $version = $tool.split("=")
 
+    # verify that the version we expect is what was restored
+    $pkgVerPath = Join-Path $packagesPath "$name\$version"
+    if ((Test-Path $pkgVerPath) -eq 0)
+    {
+        Write-Output "Directory '$pkgVerPath' doesn't exist, ensure that the version restored matches the version specified."
+        exit 1
+    }
+
     # at present we have the following conventions when staging package content:
     #   1.  if a package contains a "tools" directory then recursively copy its contents
     #       to a directory named the package ID that's under $ToolsLocalPath.
@@ -93,20 +105,20 @@ foreach ($tool in $tools)
     #       under the $ToolsLocalPath directory.
     #   3.  if a package contains a file "lib\init-tools.cmd" execute it.
 
-    if (Test-Path (Join-Path $packagesPath "$name\$version\tools"))
+    if (Test-Path (Join-Path $pkgVerPath "tools"))
     {
         $destination = Join-Path $ToolsLocalPath $name
         mkdir $destination | Out-Null
-        copy (Join-Path $packagesPath "$name\$version\tools\*") $destination -recurse
+        copy (Join-Path $pkgVerPath "tools\*") $destination -recurse
     }
-    elseif (Test-Path (Join-Path $packagesPath "$name\$version\lib"))
+    elseif (Test-Path (Join-Path $pkgVerPath "lib"))
     {
-        copy (Join-Path $packagesPath "$name\$version\lib\*") $ToolsLocalPath -recurse
+        copy (Join-Path $pkgVerPath "lib\*") $ToolsLocalPath -recurse
     }
 
-    if (Test-Path (Join-Path $packagesPath "$name\$version\lib\init-tools.cmd"))
+    if (Test-Path (Join-Path $pkgVerPath "lib\init-tools.cmd"))
     {
-        cmd.exe /c (Join-Path $packagesPath "$name\$version\lib\init-tools.cmd") $RepositoryRoot $dotNetExe $ToolsLocalPath | Out-File (Join-Path $RepositoryRoot "Init-$name.log")
+        cmd.exe /c (Join-Path $pkgVerPath "lib\init-tools.cmd") $RepositoryRoot $dotNetExe $ToolsLocalPath | Out-File (Join-Path $RepositoryRoot "Init-$name.log")
     }
 }
 
---
+++ run.ps1
@@ -0,0 +1,29 @@
+# set the base tools directory
+$toolsLocalPath = Join-Path $PSScriptRoot "Tools"
+$bootStrapperPath = Join-Path $toolsLocalPath "bootstrap.ps1"
+
+# if the boot-strapper script doesn't exist then download it
+if ((Test-Path $bootStrapperPath) -eq 0)
+{
+    if ((Test-Path $toolsLocalPath) -eq 0)
+    {
+        mkdir $toolsLocalPath | Out-Null
+    }
+
+    # download boot-strapper script
+    Invoke-WebRequest "https://raw.githubusercontent.com/dotnet/buildtools/master/bootstrap/bootstrap.ps1" -OutFile $bootStrapperPath
+}
+
+# now execute it
+& $bootStrapperPath (Get-Location) $toolsLocalPath | Out-File (Join-Path (Get-Location) "bootstrap.log")
+if ($LastExitCode -ne 0)
+{
+    Write-Output "Boot-strapping failed with exit code $LastExitCode, see bootstrap.log for more information."
+    exit $LastExitCode
+}
+
+# execute the tool using the dotnet.exe host
+$dotNetExe = Join-Path $toolsLocalPath "dotnetcli\shared\Microsoft.NETCore.App\version\dotnet.exe"
+$runExe = Join-Path $toolsLocalPath "Microsoft.DotNet.BuildTools.Run\netcoreapp1.0\run.exe"
+& $dotNetExe $runExe $args
+exit $LastExitCode
