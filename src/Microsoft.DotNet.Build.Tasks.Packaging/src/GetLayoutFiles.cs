13469921a17a045861bd927a61c41494f46f77cb
Eric St. John <ericstj@microsoft.com>
2016-10-14 12:54:23 -0700

    Generate layout from package reports

---
+++ src/Microsoft.DotNet.Build.Tasks.Packaging/src/GetLayoutFiles.cs
@@ -0,0 +1,139 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using NuGet.Frameworks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Microsoft.DotNet.Build.Tasks.Packaging
+{
+    public class GetLayoutFiles : PackagingTask
+    {
+        /// <summary>
+        /// Package report files
+        /// </summary>
+        [Required]
+        public string[] PackageReports { get; set; }
+
+        /// <summary>
+        /// Destination directory for layout
+        /// </summary>
+        [Required]
+        public string DestinationDirectory { get; set; }
+
+        /// <summary>
+        /// Optional set of frameworks to restrict the layout
+        ///   Identity: Framework
+        ///   RuntimeIDs: Semi-colon seperated list of runtime IDs
+        /// </summary>
+        public ITaskItem[] Frameworks { get; set; }
+
+        /// <summary>
+        /// Files to be copied to layout
+        ///   Identity : source path
+        ///   Destination : destination path
+        /// </summary>
+        [Output]
+        public ITaskItem[] LayoutFiles { get; set; }
+        
+        public override bool Execute()
+        {
+            var frameworks = Frameworks.NullAsEmpty().ToDictionary(
+                i => NuGetFramework.Parse(i.ItemSpec),
+                i => 
+                {
+                    var rids = i.GetMetadata("RuntimeIds");
+                    return String.IsNullOrEmpty(rids) ? new HashSet<string>() : new HashSet<string>(rids.Split(';'));
+                },
+                NuGetFramework.Comparer);
+
+            var layoutFiles = new List<ITaskItem>();
+
+            foreach(var packageReportFile in PackageReports)
+            {
+                var packageReport = PackageReport.Load(packageReportFile);
+
+                foreach(var targetInfo in packageReport.Targets)
+                {
+                    var targetName = targetInfo.Key;
+                    var target = targetInfo.Value;
+
+                    var targetParts = targetName.Split('/');
+
+                    var fx = NuGetFramework.Parse(targetParts[0]);
+                    string rid = null;
+
+                    if (targetParts.Length > 1)
+                    {
+                        rid = targetParts[1];
+                    }
+                    
+                    if (frameworks.Count != 0)
+                    {
+                        HashSet<string> rids = null;
+
+                        if (!frameworks.TryGetValue(fx, out rids))
+                        {
+                            Log.LogMessage(LogImportance.Low, $"Skipping {fx} since it is not in {nameof(Frameworks)}");
+                            continue;
+                        }
+
+                        if (rid != null && rids.Count > 0 && !rids.Contains(rid))
+                        {
+                            Log.LogMessage(LogImportance.Low, $"Skipping {fx}/{rid} since it is not in {nameof(Frameworks)}");
+                            continue;
+                        }
+                    }
+
+                    if (!packageReport.SupportedFrameworks.ContainsKey(fx.ToString()))
+                    {
+                        Log.LogMessage(LogImportance.Low, $"Skipping {fx} since it is not supported");
+                        continue;
+                    }
+
+                    if (rid == null)
+                    {
+                        // only consider compile assets for RID-less target
+                        foreach (var compileAsset in target.CompileAssets.Where(pa => !NuGetAssetResolver.IsPlaceholder(pa.LocalPath)))
+                        {
+                            layoutFiles.Add(CreateLayoutFile(compileAsset.LocalPath, "ref", fx));
+                        }
+                    }
+
+                    var runtimeAssets = target.RuntimeAssets.Concat(target.NativeAssets);
+                    foreach (var runtimeAsset in runtimeAssets.Where(pa => !NuGetAssetResolver.IsPlaceholder(pa.LocalPath)))
+                    {
+                        layoutFiles.Add(CreateLayoutFile(runtimeAsset.LocalPath, "runtime", fx, rid));
+                    }
+                }
+            }
+
+            LayoutFiles = layoutFiles.ToArray();
+
+            return !Log.HasLoggedErrors;
+        }
+
+        private ITaskItem CreateLayoutFile(string source, string subfolder, NuGetFramework framework, string rid = null)
+        {
+            var item = new TaskItem(source);
+
+            var destination = Path.Combine(DestinationDirectory, subfolder, framework.GetShortFolderName());
+
+            if (rid != null)
+            {
+                destination = Path.Combine(destination, rid);
+            }
+
+            destination = Path.Combine(destination, Path.GetFileName(source));
+
+            item.SetMetadata("Destination", destination);
+
+            return item;
+        }
+    }
+}
