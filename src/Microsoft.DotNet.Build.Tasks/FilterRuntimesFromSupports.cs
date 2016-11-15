29ad672439d8961edad66d5b0cb900b36b5cd9d1
Karthik Rajasekaran <karajas@microsoft.com>
2016-11-14 18:33:49 -0800

    Move detect default targets into Build.Common.targets
    cleanup filterruntimefromsupports imports

--- src/Microsoft.DotNet.Build.Tasks/FilterRuntimesFromSupports.cs
+++ src/Microsoft.DotNet.Build.Tasks/FilterRuntimesFromSupports.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
-using NuGet.Frameworks;
 using NuGet.RuntimeModel;
 
 namespace Microsoft.DotNet.Build.Tasks
@@ -37,8 +39,7 @@ namespace Microsoft.DotNet.Build.Tasks
         public static List<TfmRidPair> FilterForApplicableTFMRIDPairs(List<TfmRidPair> tfmRidPairs,
             string framework, string runtimeIdentifier)
         {
-            tfmRidPairs.RemoveAll(x => !framework.Equals(x.framework));
-            tfmRidPairs.RemoveAll(x => !runtimeIdentifier.Equals(x.runtimeIdentifier));
+            tfmRidPairs.RemoveAll(x => !framework.Equals(x.framework) || !runtimeIdentifier.Equals(x.runtimeIdentifier));
             
             return tfmRidPairs;
         }
