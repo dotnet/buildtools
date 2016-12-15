c3f717b058a00786110107a65e24c2254c0f4ea5
Karthik Rajasekaran <karajas@microsoft.com>
2016-12-14 16:52:39 -0800

    Filter native assemblies from binding redirect (#1263)

---
+++ src/Microsoft.DotNet.Build.Tasks/FilterNativeAssemblies.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
+using Microsoft.Build.Framework;
+using Task = Microsoft.Build.Utilities.Task;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    public class FilterNativeAssemblies : Task
+    {
+        /// <summary>
+        /// An item group of assemblies to be checked whether they are managed or native
+        /// </summary>
+        [Required]
+        public ITaskItem[] Assemblies { get; set; }
+
+        /// <summary>
+        /// An item group containing only managed assemblies
+        /// </summary>
+        [Output]
+        public ITaskItem[] ManagedAssemblies { get; set; }
+
+        public override bool Execute()
+        {
+            var assemblies = new List<ITaskItem>();
+
+            foreach (ITaskItem assembly in Assemblies)
+            {
+                try
+                {
+                    using (var filestream = new FileStream(assembly.ItemSpec, FileMode.Open, FileAccess.Read))
+                    using (PEReader peReader = new PEReader(filestream))
+                    {
+                        if (peReader.HasMetadata)
+                        {
+                            MetadataReader reader = peReader.GetMetadataReader();
+                            if (reader.IsAssembly)
+                            {
+                                assemblies.Add(assembly);
+                            }
+                        }
+                        else
+                        {
+                            //Native
+                            continue;
+                        }
+                    }
+                }
+                catch (BadImageFormatException)
+                {
+                    //Unknown
+                    continue;
+                }
+            }
+
+            ManagedAssemblies = assemblies.ToArray();
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
