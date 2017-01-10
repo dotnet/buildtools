b72ecb72e8f83e515eadcd8a51dbd098115b4a0c
Matt Galbraith <mattgal@microsoft.com>
2017-01-09 16:02:47 -0800

    Add script that can do retry when git clone-ing.

---
+++ src/Microsoft.DotNet.Build.Tasks/PackageFiles/scripts/vstsagent/CloneWithRetry.cmd
@@ -0,0 +1,36 @@
+@echo off
+
+if "%1" == ""   goto :usage
+if "%2" == ""   goto :usage
+goto :main
+
+:usage
+echo Usage: %~n0 [repository url] [destination folder]
+echo.
+echo Clones a git repo using the specified URL with depth 1 (for performance), 
+echo retrying up to 10x if there is a failure.
+goto :eof
+
+:main
+set REPO_URL=%1
+set FOLDER_NAME=%2
+set /a RETRY_COUNT=10
+set /a SLEEP_TIME=10
+set /a CLONE_DEPTH=1
+
+IF NOT DEFINED GIT SET "GIT=C:\Program Files (x86)\Git\cmd\git.exe"
+
+:while
+call "%GIT%" clone %REPO_URL% %FOLDER_NAME% --depth %CLONE_DEPTH%
+if %ERRORLEVEL% EQU 0 goto :done
+:failed
+set /a RETRY_COUNT-=1
+echo Clone failed.  Removing %FOLDER_NAME% in an attempt to get clone working, will retry in ~%SLEEP_TIME% seconds.
+rd /s /q %FOLDER_NAME%
+ping -n %SLEEP_TIME% 127.0.0.1 > NUL
+
+if %RETRY_COUNT% EQU 0 goto :done
+goto :while
+
+
+:done
