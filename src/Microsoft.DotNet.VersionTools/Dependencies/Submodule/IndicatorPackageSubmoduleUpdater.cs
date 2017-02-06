3fb7e465331cc43ff95ddfa4515ef2ebc6979256
Davis Goodin <dagood@microsoft.com>
2017-02-06 09:18:12 -0600

    Add submodule updaters to VersionTools

---
+++ src/Microsoft.DotNet.VersionTools/Dependencies/Submodule/IndicatorPackageSubmoduleUpdater.cs
@@ -0,0 +1,96 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using NuGet.Packaging.Core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Microsoft.DotNet.VersionTools.Dependencies.Submodule
+{
+    /// <summary>
+    /// Downloads a package specified from a dependency build info and updates the target git
+    /// submodule to match the version inside.
+    /// </summary>
+    public class IndicatorPackageSubmoduleUpdater : SubmoduleUpdater
+    {
+        /// <summary>
+        /// The NuGet v2 base url to use to download the indicator package, without a trailing '/'.
+        /// For example, https://dotnet.myget.org/F/dotnet-core/api/v2/package.
+        /// </summary>
+        public string PackageDownloadBaseUrl { get; set; }
+
+        public string IndicatorPackageId { get; }
+
+        public IndicatorPackageSubmoduleUpdater(string indicatorPackageId)
+        {
+            if (indicatorPackageId == null)
+            {
+                throw new ArgumentNullException(nameof(indicatorPackageId), "An indicator package must be specified.");
+            }
+            IndicatorPackageId = indicatorPackageId;
+        }
+
+        protected override string GetDesiredCommitHash(
+            IEnumerable<DependencyBuildInfo> dependencyBuildInfos,
+            out IEnumerable<DependencyBuildInfo> usedBuildInfos)
+        {
+            foreach (var info in dependencyBuildInfos)
+            {
+                PackageIdentity package = info.Packages
+                    .FirstOrDefault(p => p.Id == IndicatorPackageId);
+
+                if (package == null)
+                {
+                    continue;
+                }
+
+                using (ZipArchive archive = DownloadPackageAsync(info, package).Result)
+                {
+                    Stream versionTxt = archive.GetEntry("version.txt")?.Open();
+                    if (versionTxt == null)
+                    {
+                        Trace.TraceWarning(
+                            $"Downloaded '{package}' in '{info.BuildInfo.Name}' " +
+                            $"to upgrade '{Path}', but it had no version.txt file. Skipping.");
+                        continue;
+                    }
+
+                    string packageCommitHash = new StreamReader(versionTxt).ReadLine();
+                    Trace.TraceInformation($"Found commit '{packageCommitHash}' in versions.txt.");
+
+                    usedBuildInfos = new[] { info };
+                    return packageCommitHash;
+                }
+            }
+
+            Trace.TraceError($"Failed to find '{IndicatorPackageId}' specifying a commit in any build-info.");
+            usedBuildInfos = Enumerable.Empty<DependencyBuildInfo>();
+            return null;
+        }
+
+        protected async Task<ZipArchive> DownloadPackageAsync(DependencyBuildInfo info, PackageIdentity package)
+        {
+            if (PackageDownloadBaseUrl == null)
+            {
+                throw new NotSupportedException(
+                    $"A {nameof(PackageDownloadBaseUrl)} must be configured, " +
+                    "as build-infos do not have package feed details.");
+            }
+
+            string downloadUrl = $"{PackageDownloadBaseUrl}/package/{package.Id}/{package.Version}";
+            Trace.TraceInformation($"Downloading '{package}' from '{downloadUrl}'");
+
+            var client = new HttpClient();
+            Stream nupkgStream = await client.GetStreamAsync(downloadUrl);
+
+            return new ZipArchive(nupkgStream);
+        }
+    }
+}
---
+++ src/Microsoft.DotNet.VersionTools/Dependencies/Submodule/LatestCommitSubmoduleUpdater.cs
@@ -0,0 +1,68 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.DotNet.VersionTools.Dependencies.Submodule
+{
+    /// <summary>
+    /// Updates the submodule at the given path to the latest commit available from its target
+    /// repository.
+    /// </summary>
+    public class LatestCommitSubmoduleUpdater : SubmoduleUpdater
+    {
+        public string Repository { get; }
+
+        public string Ref { get; }
+
+        public LatestCommitSubmoduleUpdater(string repository, string @ref)
+        {
+            if (string.IsNullOrEmpty(repository))
+            {
+                throw new ArgumentException(
+                    "A repository must be specified. For example, 'origin'. Got null or empty string.",
+                    nameof(repository));
+            }
+            Repository = repository;
+
+            if (string.IsNullOrEmpty(@ref))
+            {
+                throw new ArgumentException(
+                    "A ref must be specified. For example, 'master'. Got null or empty string.",
+                    nameof(@ref));
+            }
+            Ref = @ref;
+        }
+
+        protected override string GetDesiredCommitHash(
+            IEnumerable<DependencyBuildInfo> dependencyBuildInfos,
+            out IEnumerable<DependencyBuildInfo> usedBuildInfos)
+        {
+            usedBuildInfos = Enumerable.Empty<DependencyBuildInfo>();
+
+            string remoteRefOutput = FetchGitOutput("ls-remote", Repository, Ref);
+
+            string[] remoteRefLines = remoteRefOutput
+                .Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(line => !string.IsNullOrWhiteSpace(line))
+                .ToArray();
+
+            if (remoteRefLines.Length != 1)
+            {
+                string allRefs = "";
+                if (remoteRefLines.Length > 1)
+                {
+                    allRefs = $" ({string.Join(", ", remoteRefLines)})";
+                }
+                throw new NotSupportedException(
+                    "The configured Ref must match exactly one ref on the remote. " +
+                    $"Matched {remoteRefLines.Length}{allRefs}.");
+            }
+
+            return remoteRefLines.Single().Split('\t').First();
+        }
+    }
+}
