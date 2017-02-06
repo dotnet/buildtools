0c3089fabd07c44873d6a59a6c6b9f5be5cd8ea8
Davis Goodin <dagood@microsoft.com>
2017-02-06 17:26:47 -0600

    Get and use IBCMerge tooling from internal feed
    This adds "internal-tool-runtime" restore just before Sync for tooling that needs to come from an authed NuGet feed. On Windows with full framework, a NuGet.Config is created on disk with an encrypted password. On *nix with Core, the unimplemented approach is to pipe a in-memory NuGet.Config into the restore command that has the credentials in plaintext. (The type of user encryption used on Windows isn't supported xplat, and the piping workaround doesn't work on Windows.) The hope in the future is to implement both methods as part of the bootstrap flow, once CoreFX uses it.
    
    IBCMerge is the first tool that we need to restore this way. I changed the optimization targets to pull it in using the new internal tool restore flow. I made some changes to support the new file type we're using to move IBC data into the build. The IBC data package id, version, and source are all configurable by the repository using the optimization targets, so auto-upgrade can happen independent of buildtools.

---
+++ src/Microsoft.DotNet.Build.Tasks/GenerateEncryptedNuGetConfig.cs
@@ -0,0 +1,74 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using NuGet.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    public class GenerateEncryptedNuGetConfig : Task
+    {
+        [Required]
+        public string ConfigPath { get; set; }
+
+        /// <summary>
+        /// Sources to configure. ItemSpec is source name, required metadata "Uri" with optional
+        /// "Username" and "Password".
+        /// </summary>
+        [Required]
+        public ITaskItem[] Sources { get; set; }
+
+        public override bool Execute()
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(ConfigPath));
+            File.WriteAllText(ConfigPath, GenerateUnencryptedNuGetConfig.CreateXmlString(null));
+
+            var settings = new Settings(
+                Path.GetDirectoryName(ConfigPath),
+                Path.GetFileName(ConfigPath));
+
+            var sourceProvider = new PackageSourceProvider(settings);
+
+            // Add source with username and encrypted password.
+            // See https://github.com/NuGet/NuGet.Client/blob/04f9b91fb/src/NuGet.Clients/NuGet.CommandLine/Commands/SourcesCommand.cs#L147-L168
+            List<PackageSource> sourceList = sourceProvider.LoadPackageSources().ToList();
+
+            foreach (ITaskItem newSourceItem in Sources)
+            {
+                string name = newSourceItem.ItemSpec;
+                string uri = newSourceItem.GetMetadata("Uri");
+                if (string.IsNullOrEmpty(uri))
+                {
+                    throw new ArgumentException(
+                        $"Item '{newSourceItem}': 'Uri' metadata required, found '{uri}'.");
+                }
+
+                var newSource = new PackageSource(uri, name);
+
+                string username = newSourceItem.GetMetadata("Username");
+                string password = newSourceItem.GetMetadata("Password");
+
+                if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+                {
+                    newSource.Credentials = PackageSourceCredential.FromUserInput(
+                        name,
+                        username,
+                        password,
+                        storePasswordInClearText: false);
+                }
+
+                sourceList.Add(newSource);
+            }
+
+            sourceProvider.SavePackageSources(sourceList);
+
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
