0f2c49445bb42e16bd5d2569cd8754c31946e854
Immo Landwerth <immol@microsoft.com>
2017-02-14 14:10:20 -0800

    Add helper functions for ApiKind

--- src/Microsoft.Cci.Extensions/Extensions/ApiKindExtensions.cs
+++ src/Microsoft.Cci.Extensions/Extensions/ApiKindExtensions.cs
@@ -21,5 +21,56 @@ namespace Microsoft.Cci.Extensions
                     return false;
             }
         }
+
+        public static bool IsNamespace(this ApiKind kind)
+        {
+            return kind == ApiKind.Namespace;
+        }
+
+        public static bool IsType(this ApiKind kind)
+        {
+            switch (kind)
+            {
+                case ApiKind.Interface:
+                case ApiKind.Delegate:
+                case ApiKind.Enum:
+                case ApiKind.Struct:
+                case ApiKind.Class:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        public static bool IsMember(this ApiKind kind)
+        {
+            switch (kind)
+            {
+                case ApiKind.EnumField:
+                case ApiKind.DelegateMember:
+                case ApiKind.Field:
+                case ApiKind.Property:
+                case ApiKind.Event:
+                case ApiKind.Constructor:
+                case ApiKind.PropertyAccessor:
+                case ApiKind.EventAccessor:
+                case ApiKind.Method:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        public static bool IsAccessor(this ApiKind kind)
+        {
+            switch (kind)
+            {
+                case ApiKind.PropertyAccessor:
+                case ApiKind.EventAccessor:
+                    return true;
+                default:
+                    return false;
+            }
+        }
     }
 }
