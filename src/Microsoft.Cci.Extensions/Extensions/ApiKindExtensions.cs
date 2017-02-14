1df7d8d0ba698401e7a09f17c626a9a467499a57
Immo Landwerth <immol@microsoft.com>
2017-02-14 14:03:57 -0800

    Move GetApiKind to TypeExtensions
    Technically, these methods ITypeDefiniton anyway, but more importantly
    by making sure the ApiKindExtensions doesn't take a dependency on CCI
    I can include this file into other project, such as our web site.

--- src/Microsoft.Cci.Extensions/Extensions/ApiKindExtensions.cs
+++ src/Microsoft.Cci.Extensions/Extensions/ApiKindExtensions.cs
@@ -8,57 +8,6 @@ namespace Microsoft.Cci.Extensions
 {
     public static class ApiKindExtensions
     {
-        public static ApiKind GetApiKind(this ITypeDefinition type)
-        {
-            return type.IsInterface
-                ? ApiKind.Interface
-                : type.IsDelegate
-                    ? ApiKind.Delegate
-                    : type.IsEnum
-                        ? ApiKind.Enum
-                        : type.IsStruct
-                            ? ApiKind.Struct
-                            : ApiKind.Class;
-        }
-
-        public static ApiKind GetApiKind(this ITypeDefinitionMember member)
-        {
-            if (member.ContainingTypeDefinition.IsDelegate)
-                return ApiKind.DelegateMember;
-
-            var field = member as IFieldDefinition;
-            if (field != null)
-            {
-                if (member.ContainingTypeDefinition.IsEnum && field.IsSpecialName)
-                    return ApiKind.EnumField;
-
-                return ApiKind.Field;
-            }
-
-            if (member is IPropertyDefinition)
-                return ApiKind.Property;
-
-            if (member is IEventDefinition)
-                return ApiKind.Event;
-
-            var method = (IMethodDefinition)member;
-            if (method.IsConstructor || method.IsStaticConstructor)
-                return ApiKind.Constructor;
-
-            var accessorType = method.GetAccessorType();
-            switch (accessorType)
-            {
-                case AccessorType.PropertyGetter:
-                case AccessorType.PropertySetter:
-                    return ApiKind.PropertyAccessor;
-                case AccessorType.EventAdder:
-                case AccessorType.EventRemover:
-                    return ApiKind.EventAccessor;
-                default:
-                    return ApiKind.Method;
-            }
-        }
-
         public static bool IsInfrastructure(this ApiKind kind)
         {
             switch (kind)
