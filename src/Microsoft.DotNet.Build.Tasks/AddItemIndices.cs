243651fe70252de5ccfd929f1644098f52afccae
Davis Goodin <dagood@users.noreply.github.com>
2017-02-16 09:32:28 -0600

    Add symbol server request submission targets (#1333)
    Symbols.targets can submit to the Microsoft symbol server, and includes the logic to extract symbol packages and publish Windows symbols to a file share. If `SymbolsRequestDryRun` is `true`, the request is created but not submitted. This is useful for trying out the targets.

---
+++ src/Microsoft.DotNet.Build.Tasks/AddItemIndices.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.Linq;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    /// <summary>
+    /// Takes Input, adds Index metadata with each item's location in the array, and outputs them.
+    /// </summary>
+    public class AddItemIndices : Task
+    {
+        [Required]
+        public ITaskItem[] Input { get; set; }
+
+        [Output]
+        public ITaskItem[] Output { get; set; }
+
+        public override bool Execute()
+        {
+            Output = Input
+                .Select((item, i) =>
+                {
+                    ITaskItem itemWithIndex = new TaskItem(item);
+                    itemWithIndex.SetMetadata("Index", i.ToString());
+                    return itemWithIndex;
+                })
+                .ToArray();
+
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
