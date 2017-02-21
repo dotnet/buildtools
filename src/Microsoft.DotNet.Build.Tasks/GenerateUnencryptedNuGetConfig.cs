c66dcc1ab127a5932ed89934f5d9e17006bb507f
Davis Goodin <dagood@users.noreply.github.com>
2017-02-21 14:10:19 -0600

    Get and use IBCMerge tooling from internal/authed VSTS feed (#1322)
    * Get and use IBCMerge tooling from internal feed
    
    This adds "internal-tool-runtime" restore just before Sync for tooling that needs to come from an authed NuGet feed. On Windows with full framework, a NuGet.Config is created on disk with an encrypted password. On *nix with Core, the unimplemented approach is to pipe a in-memory NuGet.Config into the restore command that has the credentials in plaintext. (The type of user encryption used on Windows isn't supported xplat, and the piping workaround doesn't work on Windows.) The hope in the future is to implement both methods as part of the bootstrap flow, once CoreFX uses it.
    
    IBCMerge is the first tool that we need to restore this way. I changed the optimization targets to pull it in using the new internal tool restore flow. I made some changes to support the new file type we're using to move IBC data into the build. The IBC data package id, version, and source are all configurable by the repository using the optimization targets, so auto-upgrade can happen independent of buildtools.
    
    * Review: remove pointless null/empty arg echo

---
+++ src/Microsoft.DotNet.Build.Tasks/GenerateUnencryptedNuGetConfig.cs
@@ -0,0 +1,49 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    public class GenerateUnencryptedNuGetConfig : Task
+    {
+        private const string NotImplementedMessage =
+            "Unencrypted NuGet.Config functionality is unavailable. Use an encrypted " +
+            "NuGet.Config instead. (Only available on desktop framework.)";
+
+        [Required]
+        public ITaskItem[] Sources { get; set; }
+
+        [Output]
+        public string NuGetConfigContent { get; set; }
+
+        public override bool Execute()
+        {
+            throw new NotImplementedException(NotImplementedMessage);
+        }
+
+        /// <summary>
+        /// Create a NuGet.Config XML string set up for the given source items.
+        /// </summary>
+        public static string CreateXmlString(IEnumerable<ITaskItem> sources)
+        {
+            var nuGetConfig = new XmlDocument();
+
+            XmlElement configuration = nuGetConfig.CreateElement("configuration");
+            nuGetConfig.AppendChild(configuration);
+
+            if (sources != null && sources.Any())
+            {
+                throw new NotImplementedException(NotImplementedMessage);
+            }
+
+            return nuGetConfig.OuterXml;
+        }
+    }
+}
