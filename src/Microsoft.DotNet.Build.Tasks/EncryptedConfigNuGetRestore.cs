69f23c72b27fee520e5973498f2f82b3737149a2
Davis Goodin <dagood@users.noreply.github.com>
2017-02-21 14:10:19 -0600

    Get and use IBCMerge tooling from internal/authed VSTS feed (#1322)
    * Get and use IBCMerge tooling from internal feed
    
    This adds "internal-tool-runtime" restore just before Sync for tooling that needs to come from an authed NuGet feed. On Windows with full framework, a NuGet.Config is created on disk with an encrypted password. On *nix with Core, the unimplemented approach is to pipe a in-memory NuGet.Config into the restore command that has the credentials in plaintext. (The type of user encryption used on Windows isn't supported xplat, and the piping workaround doesn't work on Windows.) The hope in the future is to implement both methods as part of the bootstrap flow, once CoreFX uses it.
    
    IBCMerge is the first tool that we need to restore this way. I changed the optimization targets to pull it in using the new internal tool restore flow. I made some changes to support the new file type we're using to move IBC data into the build. The IBC data package id, version, and source are all configurable by the repository using the optimization targets, so auto-upgrade can happen independent of buildtools.
    
    * Review: remove pointless null/empty arg echo
    
    # Conflicts:
    #	src/Microsoft.DotNet.Build.Tasks.Packaging/src.Desktop/project.json
    #	src/Microsoft.DotNet.Build.Tasks.Packaging/src/project.json
    #	src/Microsoft.DotNet.Build.Tasks.net45/Microsoft.DotNet.Build.Tasks.net45.csproj
    #	src/Microsoft.DotNet.Build.Tasks.net45/project.json
    #	src/Microsoft.DotNet.Build.Tasks/Microsoft.DotNet.Build.Tasks.csproj
    #	src/Microsoft.DotNet.Build.Tasks/project.json

---
+++ src/Microsoft.DotNet.Build.Tasks/EncryptedConfigNuGetRestore.cs
@@ -0,0 +1,52 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using NuGet.Commands;
+using NuGet.Protocol.Core.Types;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    /// <summary>
+    /// Use NuGet.Commands manually to perform a NuGet restore. This allows us to restore on the
+    /// desktop framework on Windows, where encryption is available.
+    /// 
+    /// An alternative is to acquire nuget.exe, but it's heavier than NuGet.Commands and is harder
+    /// to use in the build.
+    /// </summary>
+    public partial class EncryptedConfigNuGetRestore : Task
+    {
+        public ITaskItem[] Inputs { get; set; }
+
+        public string ConfigFile { get; set; }
+
+        public string PackagesDir { get; set; }
+
+        public override bool Execute()
+        {
+            var args = new RestoreArgs
+            {
+                Inputs = Inputs.Select(item => item.ItemSpec).ToList(),
+                ConfigFile = ConfigFile,
+                GlobalPackagesFolder = PackagesDir,
+
+                Sources = new List<string>(),
+                FallbackSources = new List<string>(),
+                CacheContext = new SourceCacheContext(),
+                RequestProviders = new List<IRestoreRequestProvider>
+                {
+                    new ProjectJsonRestoreRequestProvider(new RestoreCommandProvidersCache())
+                },
+                Log = new NugetMsBuildLogger(new TaskLoggingHelper(this))
+            };
+
+            RestoreRunner.Run(args).Wait();
+
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
