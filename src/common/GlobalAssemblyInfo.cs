e66ae84e7ecc995e51dae77cc08f783657d6d2d4
Rich Lander <rlander@microsoft.com>
2017-03-09 13:50:25 -0800

    Add 3PN (#1371)
    * Add 3PN for xUnit.net
    
    * Update file to latest xunit

--- src/common/GlobalAssemblyInfo.cs
+++ src/common/GlobalAssemblyInfo.cs
@@ -1,9 +1,9 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 
-[assembly: AssemblyCompany("Outercurve Foundation")]
+[assembly: AssemblyCompany(".NET Foundation")]
 [assembly: AssemblyProduct("xUnit.net Testing Framework")]
-[assembly: AssemblyCopyright("Copyright (C) Outercurve Foundation")]
+[assembly: AssemblyCopyright("Copyright (C) .NET Foundation")]
 [assembly: AssemblyVersion("99.99.99.0")]
 
 [assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Xunit.Sdk")]
