974d2901261a7043fd3f9f8728714d5a7bdda2a5
Jose Perez Rodriguez <joperezr@microsoft.com>
2017-05-15 17:44:25 -0500

    Adding task that will read netcoreapp versions file to get out repo's hashes (#1497)
    * Adding task that will read netcoreapp versions file to get out repo's hashes
    
    * Make task more generic
    
    * Removing VersionKeys parameter since it is not really required

---
+++ src/Microsoft.DotNet.Build.Tasks/GetNetCoreAppVersionsFromFile.cs
@@ -0,0 +1,45 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    public class GetNetCoreAppVersionsFromFile : Task
+    {
+        [Required]
+        public string PathToVersionsFile { get; set; }
+
+        [Output]
+        public ITaskItem[] VersionHashes { get; set; }
+
+        public override bool Execute()
+        {
+            if (!File.Exists(PathToVersionsFile))
+            {
+                Log.LogError($"File {PathToVersionsFile} does not exist.");
+                return false;
+            }
+
+            List<ITaskItem> hashes = new List<ITaskItem>();
+            var versionFileLines = File.ReadAllLines(PathToVersionsFile);
+            foreach (var line in versionFileLines)
+            {
+                var match = Regex.Match(line, @"(\S.*) (\S.*)");
+                if (match.Success && match.Groups.Count > 2)
+                {
+                    var itemWithHash = new TaskItem(match.Groups[1].Value);
+                    itemWithHash.SetMetadata("VersionHash", match.Groups[2].Value);
+                    hashes.Add(itemWithHash);
+                }
+            }
+            VersionHashes = hashes.ToArray();
+            return true;
+        }
+    }
+}
