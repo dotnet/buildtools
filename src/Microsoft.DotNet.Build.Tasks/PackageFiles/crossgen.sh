c0cf35a1a39ebbec3a9dc345b6f805ed530541a8
Matt Ellis <matell@microsoft.com>
2017-06-01 16:31:19 -0700

    Pick up crossgen from the shared framework package
    Since crossgen is now in the Microsoft.NETCore.App package, we don't
    need to hardcode some CoreCLR build to restore. Instead we can just
    restore the package that corresponds to the version of the shared
    framework we have in the CLI we are using and use crossgen from there.

--- src/Microsoft.DotNet.Build.Tasks/PackageFiles/crossgen.sh
+++ src/Microsoft.DotNet.Build.Tasks/PackageFiles/crossgen.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 # Restores crossgen and runs it on all tools components.
-
-__CoreClrVersion=2.0.0-preview1-25204-02
-__MyGetFeed=https://dotnet.myget.org/F/dotnet-core/api/v3/index.json
-
 usage()
 {
     echo "crossgen.sh <directory>"
@@ -21,15 +18,13 @@ restore_crossgen()
 
     __pjDir=$__toolsDir/crossgen
     mkdir -p $__pjDir
-    echo "<Project Sdk=\"Microsoft.NET.Sdk\"><PropertyGroup><TargetFramework>netcoreapp2.0</TargetFramework><DisableImplicitFrameworkReferences>true</DisableImplicitFrameworkReferences><RuntimeIdentifiers>$__packageRid</RuntimeIdentifiers></PropertyGroup><ItemGroup><PackageReference Include=\"Microsoft.NETCore.Runtime.CoreCLR\" Version=\"$__CoreClrVersion\" /><PackageReference Include=\"Microsoft.NETCore.Platforms\" Version=\"1.1.0\" /></ItemGroup></Project>" > "$__pjDir/crossgen.csproj"
+    echo "<Project Sdk=\"Microsoft.NET.Sdk\"><PropertyGroup><TargetFramework>netcoreapp2.0</TargetFramework><DisableImplicitFrameworkReferences>true</DisableImplicitFrameworkReferences><RuntimeIdentifiers>$__packageRid</RuntimeIdentifiers></PropertyGroup><ItemGroup><PackageReference Include=\"Microsoft.NETCore.App\" Version=\"$__sharedFxVersion\" /></ItemGroup></Project>" > "$__pjDir/crossgen.csproj"
     $__dotnet restore $__pjDir/crossgen.csproj --packages $__packagesDir --source $__MyGetFeed
-    __crossgenInPackage=$__packagesDir/runtime.$__packageRid.microsoft.netcore.runtime.coreclr/$__CoreClrVersion/tools/crossgen
-    if [ ! -e $__crossgenInPackage ]; then
-        echo "The crossgen executable could not be found at "$__crossgenInPackage". Aborting crossgen.sh."
+    __crossgen=$__packagesDir/runtime.$__packageRid.microsoft.netcore.app/$__sharedFxVersion/tools/crossgen
+    if [ ! -e $__crossgen ]; then
+        echo "The crossgen executable could not be found at "$__crossgen". Aborting crossgen.sh."
         exit 1
     fi
-    cp $__crossgenInPackage $__sharedFxDir
-    __crossgen=$__sharedFxDir/crossgen
     # Executables restored with .NET Core 2.0 do not have executable permission flags. https://github.com/NuGet/Home/issues/4424
     chmod +x $__crossgen
 }
@@ -57,7 +52,8 @@ crossgen_single()
         if [[ ($__file == *.dll && -e ${__file/.dll/.ni.dll}) || ($__file == *.exe && -e ${__file/.exe/.ni.exe}) ]]; then
             echo "$__file has already been crossgen'd.  Skipping."
         else
-            $__crossgen /Platform_Assemblies_Paths $__sharedFxDir:$__toolsDir /nologo /MissingDependenciesOK /ReadyToRun $__file > /dev/null
+            set +e
+            $__crossgen /Platform_Assemblies_Paths $__sharedFxDir:$__toolsDir /JitPath $__sharedFxDir/libclrjit.$__libraryExtension /nologo /MissingDependenciesOK /ReadyToRun $__file > /dev/null
             if [ $? -eq 0 ]; then
                 __outname="${__file/.dll/.ni.dll}"
                 __outname="${__outname/.exe/.ni.exe}"
@@ -65,19 +61,21 @@ crossgen_single()
             else
                 echo "Unable to successfully compile $__file"
             fi
+            set -e
         fi
     fi
 }
 
-if [ ! -z $BUILDTOOLS_SKIP_CROSSGEN ]; then
+if [ ! -z ${BUILDTOOLS_SKIP_CROSSGEN:-} ]; then
     echo "BUILDTOOLS_SKIP_CROSSGEN is set. Skipping crossgen step."
     exit 0
 fi
 
-if [[ -z "$1" || "$1" == "-?" || "$1" == "--help" || "$1" == "-h" ]]; then
+if [[ -z "${1:-}" || "$1" == "-?" || "$1" == "--help" || "$1" == "-h" ]]; then
     usage
 fi
 
+__MyGetFeed=${BUILDTOOLS_CROSSGEN_FEED:-https://dotnet.myget.org/F/dotnet-core/api/v3/index.json}
 __targetDir=$1
 __scriptpath=$(cd "$(dirname "$0")"; pwd -P)
 __toolsDir=$__scriptpath/../Tools
@@ -90,9 +88,11 @@ __sharedFxDir=$__toolsDir/dotnetcli/shared/Microsoft.NETCore.App/$__sharedFxVers
 case $(uname -s) in
     Darwin)
         __packageRid=osx-x64
+        __libraryExtension=dylib
         ;;
     Linux)
         __packageRid=linux-x64
+        __libraryExtension=so
         ;;
     *)
         echo "Unsupported OS $(uname -s) detected. Skipping crossgen of the toolset."
