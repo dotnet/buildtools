41e4fff5b14bfd98427df0b412158f2d1912c4e8
Davis Goodin <dagood@microsoft.com>
2017-07-11 15:40:22 -0500

    Convert Portable to Windows PDBs in symbol archive

---
+++ src/Microsoft.DotNet.Build.Tasks/ConvertPdbsToPortablePdbs.cs
@@ -0,0 +1,99 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Microsoft.DiaSymReader.Tools;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    public class ConvertPdbsToPortablePdbs : BuildTask
+    {
+        private const string PdbPathMetadata = "PdbPath";
+        private const string TargetPathMetadata = "TargetPath";
+
+        [Required]
+        public ITaskItem[] Files { get; set; }
+
+        public string[] ConversionOptions { get; set; }
+
+        public override bool Execute()
+        {
+            var parsedConversionOptions = PdbConversionOptions.Default;
+            foreach (string option in ConversionOptions ?? Enumerable.Empty<string>())
+            {
+                PdbConversionOptions parsedOption;
+                if (!Enum.TryParse(option, out parsedOption))
+                {
+                    throw new ArgumentException(
+                        $"Passed conversion option '{option}'" +
+                        $"is not a value of {nameof(PdbConversionOptions)}.");
+                }
+                parsedConversionOptions |= parsedOption;
+            }
+
+            var converter = new PdbConverter(
+                d => Log.LogError(d.ToString(CultureInfo.InvariantCulture)));
+
+            foreach (ITaskItem file in Files)
+            {
+                string pdbPath = file.GetMetadata(PdbPathMetadata);
+
+                if (string.IsNullOrEmpty(pdbPath))
+                {
+                    Log.LogError($"No '{PdbPathMetadata}' metadata found for '{file}'.");
+                    continue;
+                }
+
+                string targetPath = file.GetMetadata(TargetPathMetadata);
+
+                if (string.IsNullOrEmpty(targetPath))
+                {
+                    Log.LogError($"No '{TargetPathMetadata}' metadata found for '{file}'.");
+                    continue;
+                }
+
+                using (var sourcePdbStream = new FileStream(pdbPath, FileMode.Open, FileAccess.Read))
+                {
+                    if (PdbConverter.IsPortable(sourcePdbStream))
+                    {
+                        Log.LogMessage(
+                            MessageImportance.Low,
+                            $"Converting portable PDB '{file.ItemSpec}'...");
+
+                        Directory.CreateDirectory(Path.GetDirectoryName(targetPath));
+
+                        using (var peStream = new FileStream(file.ItemSpec, FileMode.Open, FileAccess.Read))
+                        using (var peReader = new PEReader(peStream, PEStreamOptions.LeaveOpen))
+                        using (var outPdbStream = new FileStream(targetPath, FileMode.Create, FileAccess.Write))
+                        {
+                            converter.ConvertPortableToWindows(
+                                peReader,
+                                sourcePdbStream,
+                                outPdbStream,
+                                parsedConversionOptions);
+                        }
+
+                        Log.LogMessage(
+                            MessageImportance.Normal,
+                            $"Portable PDB '{file.ItemSpec}' -> '{targetPath}'");
+                    }
+                    else
+                    {
+                        Log.LogMessage(
+                            MessageImportance.Normal,
+                            $"PDB is not portable, skipping: '{file.ItemSpec}'");
+                    }
+                }
+            }
+
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
