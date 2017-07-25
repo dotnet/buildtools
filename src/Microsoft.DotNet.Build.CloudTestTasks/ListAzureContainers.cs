b81d0c442b0d326da7a7e4256c3acd2b1eaaa6b4
Matt Galbraith <mattgal@microsoft.com>
2017-07-24 18:07:11 -0700

    Support listing > 5000 containers

--- src/Microsoft.DotNet.Build.CloudTestTasks/ListAzureContainers.cs
+++ src/Microsoft.DotNet.Build.CloudTestTasks/ListAzureContainers.cs
@@ -5,7 +5,7 @@
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using System;
-using System.Globalization;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -18,7 +18,7 @@ namespace Microsoft.DotNet.Build.CloudTestTasks
         /// <summary>
         /// Prefix of Azure containers desired to return;
         /// </summary>
-        public string Prefix { get; set; }
+        public string Prefix { get; set; } = String.Empty;
 
         /// <summary>
         /// An item group of container names to download.  
@@ -44,15 +44,14 @@ namespace Microsoft.DotNet.Build.CloudTestTasks
             string url = string.Format("https://{0}.blob.core.windows.net/?comp=list", AccountName);
 
             Log.LogMessage(MessageImportance.Low, "Sending request to list containers in account '{0}'.", AccountName);
-
+            List<ITaskItem> discoveredContainers = new List<ITaskItem>();
             using (HttpClient client = new HttpClient())
             {
+                string nextMarker = string.Empty;
                 try
                 {
                     var createRequest = AzureHelper.RequestMessage("GET", url, AccountName, AccountKey);
 
-                    // TODO:  This task has a bug, it needs to continue when there are > 5000 containers in a storage acccount.
-                    //        Fix is something like the one made to DownloadFromAzure, but not pressing since it looks like GetLatestContainerNameFromAzure is rarely / not used.
                     XmlDocument responseFile;
                     using (HttpResponseMessage response = await AzureHelper.RequestWithRetry(Log, client, createRequest))
                     {
@@ -60,14 +59,29 @@ namespace Microsoft.DotNet.Build.CloudTestTasks
                         responseFile.LoadXml(await response.Content.ReadAsStringAsync());
                         XmlNodeList elemList = responseFile.GetElementsByTagName("Name");
 
-                        ContainerNames = (from x in elemList.Cast<XmlNode>()
-                                          where x.InnerText.Contains(Prefix)
-                                          select new TaskItem(x.InnerText)).ToArray();
 
-                        if (ContainerNames.Length == 0)
-                            Log.LogWarning("No containers were found.");
-                        else
-                            Log.LogMessage("Found {0} containers.", ContainerNames.Length);
+                        discoveredContainers.AddRange(from x in elemList.Cast<XmlNode>()
+                                                      where x.InnerText.Contains(Prefix)
+                                                      select new TaskItem(x.InnerText));
+
+                        nextMarker = responseFile.GetElementsByTagName("NextMarker").Cast<XmlNode>().FirstOrDefault()?.InnerText;
+                    }
+                    while (!string.IsNullOrEmpty(nextMarker))
+                    {
+                        string urlNext = string.Format($"{url}&marker={nextMarker}");
+                        var nextRequest = AzureHelper.RequestMessage("GET", urlNext, AccountName, AccountKey);
+                        using (HttpResponseMessage nextResponse = AzureHelper.RequestWithRetry(Log, client, nextRequest).GetAwaiter().GetResult())
+                        {
+                            responseFile = new XmlDocument();
+                            responseFile.LoadXml(await nextResponse.Content.ReadAsStringAsync());
+                            XmlNodeList elemList = responseFile.GetElementsByTagName("Name");
+
+                            discoveredContainers.AddRange(from x in elemList.Cast<XmlNode>()
+                                                          where x.InnerText.Contains(Prefix)
+                                                          select new TaskItem(x.InnerText));
+
+                            nextMarker = responseFile.GetElementsByTagName("NextMarker").Cast<XmlNode>().FirstOrDefault()?.InnerText;
+                        }
                     }
                 }
                 catch (Exception e)
@@ -75,6 +89,11 @@ namespace Microsoft.DotNet.Build.CloudTestTasks
                     Log.LogErrorFromException(e, true);
                 }
             }
+            ContainerNames = discoveredContainers.ToArray();
+            if (ContainerNames.Length == 0)
+                Log.LogWarning("No containers were found.");
+            else
+                Log.LogMessage("Found {0} containers.", ContainerNames.Length);
 
             return !Log.HasLoggedErrors;
         }
