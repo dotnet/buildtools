271006e3521218734b58b24f7bef7903baf298e3
jbeisner <johnbeisner@users.noreply.github.com>
2017-09-12 12:14:31 -0700

    Change the buildtools scripts to use the only supported version of the CLI installation scripts: https://dot.net/v1/dotnet-install.sh https://dot.net/v1/dotnet-install.ps1

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -80,7 +80,6 @@ sharedFxVersion="<auto>"
 force=
 forcedCliLocalPath="<none>"
 architecture="<auto>"
-dotNetInstallBranch="rel/1.0.0"
 
 while [ $# -ne 0 ]
 do
@@ -105,10 +104,6 @@ do
         -a|--architecture|-[Aa]rchitecture)
             shift
             architecture="$1"
-            ;;
-        --dotNetInstallBranch|-[Dd]ot[Nn]et[Ii]nstall[Bb]ranch)
-            shift
-            dotNetInstallBranch="$1"
             ;;
         --sharedFrameworkSymlinkPath|--symlink|-[Ss]haredFrameworkSymlinkPath)
             shift
@@ -176,8 +171,8 @@ fi
 if [ $forcedCliLocalPath = "<none>" ]; then
     check_min_reqs
 
-    # download CLI boot-strapper script
-    download "https://raw.githubusercontent.com/dotnet/cli/$dotNetInstallBranch/scripts/obtain/dotnet-install.sh" "$dotnetInstallPath"
+    # download CLI dotnet-install script
+    download "https://dot.net/v1/dotnet-install.sh" "$dotnetInstallPath"
     chmod u+x "$dotnetInstallPath"
 
     # load the version of the CLI
--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 __scriptpath=$(cd "$(dirname "$0")"; pwd -P)
 
-"$__scriptpath/bootstrap/bootstrap.sh" -r "$__scriptpath" -t "$__scriptpath/Tools" -DotNetInstallBranch "rel/1.0.0-preview2.1"
+"$__scriptpath/bootstrap/bootstrap.sh" -r "$__scriptpath" -t "$__scriptpath/Tools"
 __dotnet="$__scriptpath/Tools/dotnetcli/dotnet"
 __toolruntime="$__scriptpath/Tools"
 
