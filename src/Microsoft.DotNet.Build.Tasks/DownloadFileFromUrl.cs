043d7ee9d72622b85891a669e3389c5fbc72b1ec
Santiago Fernandez <safern@microsoft.com>
2017-09-27 18:14:12 -0700

    PR Feedback and handle proxy issue

--- src/Microsoft.DotNet.Build.Tasks/DownloadFileFromUrl.cs
+++ src/Microsoft.DotNet.Build.Tasks/DownloadFileFromUrl.cs
@@ -2,71 +2,65 @@ using Microsoft.Build.Framework;
 using System;
 using System.IO;
 using System.Net.Http;
+using System.Net;
 
 namespace Microsoft.DotNet.Build.Tasks
 {
     public sealed class DownloadFileFromUrl : BuildTask
     {
-        
         /// <summary>
         /// The url to download the file from.
         /// </summary>
         [Required]
-        public string DownloadSource { get; set;}
+        public string DownloadSource { get; set; }
         
         /// <summary>
         /// The directory where the file will be downloaded to.
         /// </summary>
         [Required]
-        public string DestinationDirectory { get; set; }
+        public string DestinationFile { get; set; }
 
         /// <summary>
-        /// The file where the source will be downloaded to.
+        /// The default is to fail on error, set this to true if you want to just warn.
         /// </summary>
-        [Required]
-        public string DestinationFileName { get; set; }
-
-        /// <summary>
-        /// The default is to emit warnings and not fail, the caller can fail if output parameter DownloadedFile is empty.
-        /// Set this to true to fail instead of just warn.
-        /// </summary>
-        public bool FailOnError { get; set; }
-
-        /// <summary>
-        /// The file that was downloaded, if there is an error this will be empty.
-        /// </summary>
-        [Output]
-        public string DownloadedFile { get; set; }
+        public bool TreatErrorsAsWarnings { get; set; }
 
         public override bool Execute()
         {
             try
             {
-                Log.LogMessage(MessageImportance.High, $"Downloading {DownloadSource} -> {DestinationFileName}");
-                Directory.CreateDirectory(DestinationDirectory);
-                string destinationPath = Path.Combine(DestinationDirectory, DestinationFileName);
-                using (FileStream stream = File.Create(destinationPath))
+                Log.LogMessage(MessageImportance.High, $"Downloading {DownloadSource} -> {DestinationFile}");
+                var directory = Path.GetDirectoryName(DestinationFile);
+                Directory.CreateDirectory(directory);
+                using (FileStream stream = File.Create(DestinationFile))
                 {
-                    using (var client = new HttpClient())
+                    using (var handler = GetHttpHandler())
                     {
-                        using (var result = client.GetAsync(DownloadSource).GetAwaiter().GetResult())
+                        using (var client = new HttpClient(handler))
                         {
-                            if (result.IsSuccessStatusCode)
+                            using (var result = client.GetAsync(DownloadSource).GetAwaiter().GetResult())
                             {
-                                var bytes = result.Content.ReadAsByteArrayAsync().GetAwaiter().GetResult();
-                                stream.Write(bytes, 0, bytes.Length);
-                                DownloadedFile = destinationPath;
-                                Log.LogMessage(MessageImportance.High, $"Finished downloading: {DownloadSource}");
-                            }
-                            else
-                            {
-                                if (FailOnError)
+                                if (result.IsSuccessStatusCode)
                                 {
-                                    Log.LogError($"Downloading {DownloadSource} failed with status code: {result.StatusCode}");                                    
+                                    result.Content.CopyToAsync(stream).GetAwaiter().GetResult();
+                                    Log.LogMessage(MessageImportance.High, $"Finished downloading: {DownloadSource}");
                                 }
                                 else
                                 {
-                                    Log.LogWarning($"Downloading {DownloadSource} failed with status code: {result.StatusCode}");
+                                    if (File.Exists(DestinationFile))
+                                    {
+                                        // If we fail to download, we want to do cleanup to not leave empty files.
+                                        File.Delete(DestinationFile);
+                                    }
+
+                                    if (TreatErrorsAsWarnings)
+                                    {
+                                        Log.LogWarning($"Downloading {DownloadSource} failed with status code: {result.StatusCode}");
+                                    }
+                                    else
+                                    {
+                                        Log.LogError($"Downloading {DownloadSource} failed with status code: {result.StatusCode}");
+                                    }
                                 }
                             }
                         }
@@ -75,18 +69,33 @@ namespace Microsoft.DotNet.Build.Tasks
             }
             catch (Exception e)
             {
-                if (FailOnError)
+                if (TreatErrorsAsWarnings)
+                {
+                    Log.LogWarningFromException(e, showStackTrace: true);
+                }
+                else
                 {
                     Log.LogError($"Downloading {DownloadSource} failed with exception: ");
                     Log.LogErrorFromException(e, showStackTrace: true);
                 }
-                else
-                {
-                    Log.LogWarningFromException(e, showStackTrace: true);
-                }
             }
 
             return !Log.HasLoggedErrors;
+        }
+
+        public HttpClientHandler GetHttpHandler()
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+#if !net45
+            handler.DefaultProxyCredentials = CredentialCache.DefaultCredentials;
+#else
+            handler.Proxy = WebRequest.DefaultWebProxy;
+
+            if (handler.Proxy != null)
+                handler.Proxy.Credentials = CredentialCache.DefaultCredentials;
+#endif // net45
+
+            return handler;
         }
     }
 }
