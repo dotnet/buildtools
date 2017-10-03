96de1957d8b83464819a7e145f2288c50c6f5b24
Christopher Costa <chcosta@microsoft.com>
2017-10-03 10:45:46 -0700

    use dotnet 2.0.0

--- vars/getDotNet.groovy
+++ vars/getDotNet.groovy
@@ -3,7 +3,7 @@
  *  Example: getDotNet dotNetUrl: "https://go.microsoft.com/fwlink/?LinkID=809115", dotNetFilename: "dotnet-win-x64"
  */
 def call(String downloadFolder = "${WORKSPACE}\\ciTools") {
-  def dotNetUrl = "https://go.microsoft.com/fwlink/?LinkID=843454"
+  def dotNetUrl = "https://download.microsoft.com/download/1/B/4/1B4DE605-8378-47A5-B01B-2C79D6C55519/dotnet-sdk-2.0.0-win-x64.zip" //"https://go.microsoft.com/fwlink/?LinkID=843454"
   def dotNetPath
   def dotNetFilename
   if(isUnix()) {
