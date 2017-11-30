d28ba99c95e0d9401d825a4222b9d6590b86dd59
Matt Mitchell <mmitche@microsoft.com>
2017-11-30 15:52:52 -0800

    Merging feed/blob tasks Two tasks are implemented in here:     1) Copies a set of blobs folders to a target blob folder     2) Parses a blob url to obtain the interesting bits of the URI (account name, container, blob path) This supports a future PR (currently staged in the pipeline repo but will be moved to buildtools.  Which merges a set of feeds and feed related items (like assets) for product construction
    Address PR feedback

---
+++ src/Microsoft.DotNet.Build.Tasks.Feed/BlobUrlInfo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Auth;
+using System.Text.RegularExpressions;
+
+namespace Microsoft.DotNet.Build.Tasks.Feed
+{
+    class BlobUrlInfo
+    {
+        private const string accountNameRegex = @"(?<accountname>[a-z0-9]+)\.blob\.core\.windows\.net.*";
+        private const string containerAndBlobRegex = @"/(?<containername>[^\/]+)/(?<blobpath>.*)";
+        public string AccountName { get; set; }
+
+        public string ContainerName { get; set; }
+
+        public string BlobPath { get; set; }
+
+        public bool HasToken { get; set; }
+
+        public Uri Uri
+        {
+            get
+            {
+                return new Uri($"https://{AccountName}.blob.core.windows.net/{ContainerName}/{BlobPath}");
+            }
+        }
+
+        public BlobUrlInfo(string url)
+            : this(new Uri(url))
+        { }
+
+        public BlobUrlInfo(Uri uri)
+        {
+            // Account name is the first element of the hostname.
+            string hostName = uri.Host;
+            Match hostNameMatch = Regex.Match(hostName, accountNameRegex);
+
+            if (hostNameMatch.Success)
+            {
+                AccountName = hostNameMatch.Groups["accountname"].Value;
+            }
+            else
+            {
+                throw new ArgumentException("Blob URL host name should be of the form <account name>.blob.core.windows.net");
+            }
+
+            String path = uri.AbsolutePath;
+            Match containerAndBlobMatch = Regex.Match(path, containerAndBlobRegex);
+
+            if (containerAndBlobMatch.Success)
+            {
+                ContainerName = containerAndBlobMatch.Groups["containername"].Value;
+                BlobPath = containerAndBlobMatch.Groups["blobpath"].Value;
+            }
+            else
+            {
+                throw new ArgumentException("Blob URL path should have a container and blob path");
+            }
+
+            // TODO, for authenticated nuget feeds using traditional query strings, we should change this
+            // to support recognition of the SAS token
+            if (!String.IsNullOrEmpty(uri.Query))
+            {
+                HasToken = true;
+                throw new NotImplementedException("Authenticated SAS token blob URIs is not yet implemented");
+            }
+        }
+
+        public BlobUrlInfo(string accountName, string containerName, string blobPath)
+        {
+            AccountName = accountName;
+            ContainerName = containerName;
+            BlobPath = blobPath;
+        }
+
+        public string GetConnectionString(string accountKey)
+        {
+            return $"DefaultEndpointsProtocol=https;AccountName={AccountName};AccountKey={accountKey};EndpointSuffix=core.windows.net";
+        }
+    }
+}
