75182e15a1007fb1a31b615f5546b420139c65c7
Jose Perez Rodriguez <joperezr@microsoft.com>
2018-01-25 12:54:57 -0800

    Changes required in order to build corefx on linux using CLI's MSBuild

--- src/Microsoft.DotNet.Build.Tasks/PackageFiles/msbuild.sh
+++ src/Microsoft.DotNet.Build.Tasks/PackageFiles/msbuild.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 working_tree_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$working_tree_root/dotnetcli/dotnet $working_tree_root/MSBuild.dll $*
+$working_tree_root/dotnetcli/dotnet msbuild $*
 exit $?
