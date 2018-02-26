c2432f11ce6c3b287362fe28b99e9bcf5e932730
danmosemsft <danmose@microsoft.com>
2018-02-26 09:33:16 -0800

    Simplest change

--- src/Microsoft.DotNet.Build.Tasks/CleanupVSTSAgent.cs
+++ src/Microsoft.DotNet.Build.Tasks/CleanupVSTSAgent.cs
@@ -595,7 +595,7 @@ namespace Microsoft.DotNet.Build.Tasks
             IntPtr hFile = FindFirstFile(directory + "\\*", out findData);
             int error = Marshal.GetLastWin32Error();
 
-            if (hFile.ToInt32() != -1)
+            if (handle != IntPtr.Zero && handle != new IntPtr(-1))
             {
                 do
                 {
