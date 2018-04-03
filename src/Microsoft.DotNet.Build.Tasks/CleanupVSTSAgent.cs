0e4245544a57b65666197bd1449a913a911130e8
Divino César <divcesar@gmail.com>
2018-04-03 14:32:49 -0700

    Bringing back VSTS CleanupAgent public interface. (#1998)

--- src/Microsoft.DotNet.Build.Tasks/CleanupVSTSAgent.cs
+++ src/Microsoft.DotNet.Build.Tasks/CleanupVSTSAgent.cs
@@ -1,7 +1,31 @@
+using Microsoft.Build.Framework;
+
 namespace Microsoft.DotNet.Build.Tasks
 {
     public class CleanupVSTSAgent : BuildTask
     {
+        public bool Clean { get; set; }	
+	
+        public bool Report { get; set; }
+
+        [Required]
+        public string AgentDirectory { get; set; }
+
+        [Required]
+        public double RetentionDays { get; set; }
+	
+        public int? Retries { get; set; }	
+	
+        public int MaximumWorkspacesToClean { get; set; } = 8;	
+	
+        public bool EnableLongPathRemoval { get; set; } = true;	
+	
+        public int? SleepTimeInMilliseconds { get; set; }	
+	
+        public ITaskItem[] ProcessNamesToKill { get; set; }	
+	
+        public string [] AdditionalCleanupDirectories { get; set; }
+
         public override bool Execute()
         {
             Log.LogWarning($"This BuildTask has been deprecated in favor of maintenance jobs.");
