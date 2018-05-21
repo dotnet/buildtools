9102d193c71d0759815d942382c0e5dc1c54dacf
dan moseley <danmose@microsoft.com>
2018-05-21 01:33:15 -0700

    Make GenFacades errors appear in MSbuild logs

--- src/GenFacades/GenFacades.Core/GenFacades.cs
+++ src/GenFacades/GenFacades.Core/GenFacades.cs
@@ -637,12 +637,15 @@ namespace GenFacades
 
             private static void TraceDuplicateSeedTypeError(string docId, IReadOnlyList<INamedTypeDefinition> seedTypes)
             {
-                Trace.TraceError("The type '{0}' is defined in multiple seed assemblies. If this is intentional, specify one of the following arguments to choose the preferred seed type:", docId);
+                var sb = new StringBuilder();
+                sb.AppendFormat("The type '{0}' is defined in multiple seed assemblies. If this is intentional, specify one of the following arguments to choose the preferred seed type:", docId);
 
                 foreach (INamedTypeDefinition type in seedTypes)
                 {
-                    Trace.TraceError("  /preferSeedType:{0}={1}", docId.Substring("T:".Length), type.GetAssembly().Name.Value);
+                    sb.AppendFormat("  /preferSeedType:{0}={1}", docId.Substring("T:".Length), type.GetAssembly().Name.Value);
                 }
+
+                Trace.TraceError(sb.ToString());
             }
 
             private void AddTypeForward(Assembly assembly, INamedTypeDefinition seedType)
