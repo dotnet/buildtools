f633286933ef7f7d01d87bef399af346c035ecad
Davis Goodin <dagood@users.noreply.github.com>
2018-06-11 12:35:16 -0700

    Implement additional ProdCon manifest updaters (#2063)
    * Build attribute from orchestrated build
     * Orchestrated blob feed attribute
     * Orchestrated blob feed package version
    
    Each updater can update a single-line file (e.g. BuildToolsVersion.txt) or make changes based on regex (with ElementName shortcut for XML).
    
    Also fixes issue with CRLF/LF handling when updating a single-line file when autocrlf isn't set consistently with the running platform.
    
    (cherry picked from commit 338f4b059ef26b1ee718aafca49cfb6cf69e7d6a)

---
+++ src/Microsoft.DotNet.VersionTools/Dependencies/BuildOutput/OrchestratedBuild/OrchestratedBuildUpdateHelpers.cs
@@ -0,0 +1,76 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.DotNet.VersionTools.BuildManifest.Model;
+using Microsoft.DotNet.VersionTools.Dependencies.BuildManifest;
+using System;
+using System.Linq;
+
+namespace Microsoft.DotNet.VersionTools.Dependencies.BuildOutput.OrchestratedBuild
+{
+    public static class OrchestratedBuildUpdateHelpers
+    {
+        public static Func<OrchestratedBuildDependencyInfo[], DependencyReplacement>
+            BuildAttribute(string buildName, string attributeName)
+        {
+            return infos =>
+            {
+                var match = OrchestratedBuildIdentityMatch.Find(buildName, infos);
+
+                string value;
+                if (match.Match.Attributes.TryGetValue(attributeName, out value))
+                {
+                    return new DependencyReplacement(value, new[] { match.Info });
+                }
+
+                return null;
+            };
+        }
+
+        public static Func<OrchestratedBuildDependencyInfo[], DependencyReplacement>
+            OrchestratedFeedAttribute(string attributeName)
+        {
+            return infos => infos
+                .Select(info =>
+                {
+                    EndpointModel feed = info.GetBlobFeed();
+
+                    string value;
+                    if (feed != null && feed.Attributes.TryGetValue(attributeName, out value))
+                    {
+                        return new DependencyReplacement(value, new[] { info });
+                    }
+
+                    return null;
+                })
+                .FirstOrDefault(r => r != null);
+        }
+
+        public static Func<OrchestratedBuildDependencyInfo[], DependencyReplacement>
+            OrchestratedFeedPackageVersion(string packageId)
+        {
+            return infos => infos
+                .Select(info =>
+                {
+                    PackageArtifactModel match = info.GetBlobFeed()?.Artifacts.Packages
+                        .FirstOrDefault(p => p.Id.Equals(packageId, StringComparison.OrdinalIgnoreCase));
+
+                    if (match != null)
+                    {
+                        return new DependencyReplacement(match.Version, new[] { info });
+                    }
+
+                    return null;
+
+                })
+                .FirstOrDefault(r => r != null);
+        }
+
+        private static EndpointModel GetBlobFeed(this OrchestratedBuildDependencyInfo info)
+        {
+            return info.OrchestratedBuildModel.Endpoints
+                .FirstOrDefault(e => e.IsOrchestratedBlobFeed);
+        }
+    }
+}
