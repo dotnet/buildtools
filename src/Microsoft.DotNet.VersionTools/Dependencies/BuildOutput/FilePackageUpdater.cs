edece9857c3350ae68fa20399a94c57445182afd
Davis Goodin <dagood@microsoft.com>
2018-06-11 11:34:15 -0500

    Implement additional ProdCon manifest updaters
     * Build attribute from orchestrated build
     * Orchestrated blob feed attribute
     * Orchestrated blob feed package version
    
    Each updater can update a single-line file (e.g. BuildToolsVersion.txt) or make changes based on regex (with ElementName shortcut for XML).
    
    Also fixes issue with CRLF/LF handling when updating a single-line file when autocrlf isn't set consistently with the running platform.
    
    (cherry picked from commit 338f4b059ef26b1ee718aafca49cfb6cf69e7d6a)

--- src/Microsoft.DotNet.VersionTools/Dependencies/BuildOutput/FilePackageUpdater.cs
+++ src/Microsoft.DotNet.VersionTools/Dependencies/BuildOutput/FilePackageUpdater.cs
@@ -2,21 +2,17 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Microsoft.DotNet.VersionTools.Util;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
 namespace Microsoft.DotNet.VersionTools.Dependencies.BuildOutput
 {
-    public class FilePackageUpdater : IDependencyUpdater
+    public class FilePackageUpdater : FileUpdater
     {
-        public string Path { get; set; }
-
         public string PackageId { get; set; }
 
-        public IEnumerable<DependencyUpdateTask> GetUpdateTasks(
+        public override DependencyReplacement GetDesiredReplacement(
             IEnumerable<IDependencyInfo> dependencyInfos)
         {
             foreach (BuildDependencyInfo info in dependencyInfos.OfType<BuildDependencyInfo>())
@@ -24,36 +20,14 @@ namespace Microsoft.DotNet.VersionTools.Dependencies.BuildOutput
                 string version;
                 if (info.RawPackages.TryGetValue(PackageId, out version))
                 {
-                    string originalValue = null;
-
-                    Action updateTask = FileUtils.GetUpdateFileContentsTask(
-                        Path,
-                        content =>
-                        {
-                            int firstLineLength = content.IndexOf(Environment.NewLine);
-                            // Handle files with no newline ending.
-                            if (firstLineLength == -1)
-                            {
-                                firstLineLength = content.Length;
-                            }
-
-                            originalValue = content.Substring(0, firstLineLength);
-                            return content
-                                .Remove(0, firstLineLength)
-                                .Insert(0, version);
-                        });
-
-                    if (updateTask != null)
-                    {
-                        yield return new DependencyUpdateTask(
-                            updateTask,
-                            new[] { info },
-                            new[] { $"In '{Path}', '{originalValue}' must be '{version}'." });
-                    }
-                    yield break;
+                    return new DependencyReplacement(
+                        version,
+                        new[] { info });
                 }
             }
+
             Trace.TraceError($"For '{Path}', Could not find '{PackageId}' package version information.");
+            return null;
         }
     }
 }
