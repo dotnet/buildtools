2d076fba78d7bcc1d8c98096ec1e293b3266f4f2
Anirudh Agnihotry <anirudhagnihotry098@gmail.com>
2019-03-01 15:26:36 -0800

    Setting the targetFramework Dynamically (#2229)
    * setting the targetFramework Dynamically
    
    * editing usage and moving tfm parameter checking with others

--- src/Microsoft.DotNet.Build.Tasks/PackageFiles/crossgen.sh
+++ src/Microsoft.DotNet.Build.Tasks/PackageFiles/crossgen.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # Restores crossgen and runs it on all tools components.
 usage()
 {
-    echo "crossgen.sh <directory> <rid>"
+    echo "crossgen.sh <directory> <rid> <targetframework>"
     echo "    Restores crossgen and runs it on all assemblies in <directory>."
     exit 0
 }
@@ -18,7 +18,7 @@ restore_crossgen()
 
     __pjDir=$__toolsDir/crossgen
     mkdir -p $__pjDir
-    echo "<Project Sdk=\"Microsoft.NET.Sdk\"><PropertyGroup><DisableImplicitNuGetFallbackFolder>false</DisableImplicitNuGetFallbackFolder><TreatWarningsAsErrors>false</TreatWarningsAsErrors><NoWarn>\$(NoWarn);NU1605;NU1103</NoWarn><TargetFramework>netcoreapp2.0</TargetFramework><DisableImplicitFrameworkReferences>true</DisableImplicitFrameworkReferences><RuntimeIdentifiers>$__packageRid</RuntimeIdentifiers></PropertyGroup><ItemGroup><PackageReference Include=\"Microsoft.NETCore.App\" Version=\"$__sharedFxVersion\" /></ItemGroup></Project>" > "$__pjDir/crossgen.csproj"
+    echo "<Project Sdk=\"Microsoft.NET.Sdk\"><PropertyGroup><DisableImplicitNuGetFallbackFolder>false</DisableImplicitNuGetFallbackFolder><TreatWarningsAsErrors>false</TreatWarningsAsErrors><NoWarn>\$(NoWarn);NU1605;NU1103</NoWarn><TargetFramework>$__TargetFramework</TargetFramework><DisableImplicitFrameworkReferences>true</DisableImplicitFrameworkReferences><RuntimeIdentifiers>$__packageRid</RuntimeIdentifiers></PropertyGroup><ItemGroup><PackageReference Include=\"Microsoft.NETCore.App\" Version=\"$__sharedFxVersion\" /></ItemGroup></Project>" > "$__pjDir/crossgen.csproj"
     $__dotnet restore $__pjDir/crossgen.csproj --packages $__packagesDir --source $__MyGetFeed
     __crossgen=$__packagesDir/runtime.$__packageRid.microsoft.netcore.app/$__sharedFxVersion/tools/crossgen
     if [ ! -e $__crossgen ]; then
@@ -78,6 +78,7 @@ fi
 __MyGetFeed=${BUILDTOOLS_CROSSGEN_FEED:-https://dotnet.myget.org/F/dotnet-core/api/v3/index.json}
 __targetDir=$1
 __packageRid=${2:-}
+__TargetFramework=${3:-}
 __scriptpath=$(cd "$(dirname "$0")"; pwd -P)
 __toolsDir=$__scriptpath/../Tools
 __dotnet=$__toolsDir/dotnetcli/dotnet
@@ -105,6 +106,10 @@ if [ "$__packageRid" == "osx-x64" ]; then
     __libraryExtension=dylib
 else
     __libraryExtension=so
+fi
+
+if [ -z "$__TargetFramework" ]; then
+    __TargetFramework="netcoreapp2.0"
 fi
 
 restore_crossgen
