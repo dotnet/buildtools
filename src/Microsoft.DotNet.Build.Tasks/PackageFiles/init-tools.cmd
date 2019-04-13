c0b83d6f5950e26732c4e6622c02d79e1b93aa66
Santiago Fernandez Madero <safern@microsoft.com>
2019-04-12 21:36:11 -0700

    Update roslyn compilers to 3.1.0-beta3-19213-02

--- src/Microsoft.DotNet.Build.Tasks/PackageFiles/init-tools.cmd
+++ src/Microsoft.DotNet.Build.Tasks/PackageFiles/init-tools.cmd
@@ -7,7 +7,7 @@ set TOOLRUNTIME_DIR=%~3
 set PACKAGES_DIR=%~4
 set BUILDTOOLS_PACKAGE_DIR=%~dp0
 set MICROBUILD_VERSION=0.2.0
-set ROSLYNCOMPILERS_VERSION=3.1.0-beta2-19206-01
+set ROSLYNCOMPILERS_VERSION=3.1.0-beta3-19213-02
 
 :: Default to x64 native tools if nothing was specified.
 if [%NATIVE_TOOLS_RID%]==[] (
